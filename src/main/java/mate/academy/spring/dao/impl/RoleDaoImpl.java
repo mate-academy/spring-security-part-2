9d3b4c73fe871733969ae458877da10db075f67d
Maksim <max.freedman.main@gmail.com>
2021-06-29 20:19:30 +0300

    Added DataInitializer bean

--- src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
@@ -8,7 +8,9 @@ import mate.academy.spring.model.Role;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
     public RoleDaoImpl(SessionFactory factory) {
         super(factory, Role.class);
--- src/main/java/mate/academy/spring/service/impl/RoleServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/RoleServiceImpl.java
@@ -4,7 +4,9 @@ import mate.academy.spring.dao.RoleDao;
 import mate.academy.spring.exception.DataProcessingException;
 import mate.academy.spring.model.Role;
 import mate.academy.spring.service.RoleService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RoleServiceImpl implements RoleService {
     private final RoleDao roleDao;
 
