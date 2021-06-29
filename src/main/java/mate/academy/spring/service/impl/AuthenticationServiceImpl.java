bd40909125fd038a71b396d3e3099bdf5b8a161b
Maksim <max.freedman.main@gmail.com>
2021-06-29 22:04:39 +0300

    Changed authentication service

--- src/main/java/mate/academy/spring/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/AuthenticationServiceImpl.java
@@ -1,19 +1,26 @@
 package mate.academy.spring.service.impl;
 
+import mate.academy.spring.model.Role;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.AuthenticationService;
+import mate.academy.spring.service.RoleService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserService userService;
+    private final RoleService roleService;
     private final ShoppingCartService shoppingCartService;
 
     public AuthenticationServiceImpl(UserService userService,
+                                     RoleService roleService,
                                      ShoppingCartService shoppingCartService) {
         this.userService = userService;
+        this.roleService = roleService;
         this.shoppingCartService = shoppingCartService;
     }
 
@@ -22,6 +29,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         User user = new User();
         user.setEmail(email);
         user.setPassword(password);
+        user.setRoles(Set.of(roleService.getRoleByName(Role.RoleName.ROLE_USER)));
         userService.add(user);
         shoppingCartService.registerNewShoppingCart(user);
         return user;
