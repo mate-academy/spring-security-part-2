af67c0749c17d7ad12889f4445e27fbbeddb24d7
Darina <to.darina.to>
2021-06-30 13:36:03 +0300

    student review1 done

--- src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
+++ src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
@@ -1,5 +1,8 @@
 package mate.academy.spring.security;
 
+import static org.springframework.security.core.userdetails.User.UserBuilder;
+import static org.springframework.security.core.userdetails.User.withUsername;
+
 import mate.academy.spring.model.Role;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
@@ -20,9 +23,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
         User user = userService.findByEmail(email);
-        org.springframework.security.core.userdetails.User.UserBuilder builder =
-                org.springframework.security.core.userdetails
-                .User.withUsername(email);
+        UserBuilder builder = withUsername(email);
         builder.password(user.getPassword());
         builder.roles(user.getRoles()
                 .stream()
