3e86bc6cfd7368d24ad6c9b8153da3bc5724fa5e
redxp <redxpertus1@gmail.com>
2021-07-02 10:11:29 +0300

    spring security p2 v02 made work with comments

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -55,9 +55,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf().disable();
     }
-
-    @Bean
-    public PasswordEncoder getEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
