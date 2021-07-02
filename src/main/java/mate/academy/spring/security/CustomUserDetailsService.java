b8ed2abd206bff0a67279ec4fab84b4c197afbf7
Andrii Romaniuk <romaniukandrii7@gmail.com>
2021-07-02 11:51:59 +0300

    mvn clean package fix

--- src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
+++ src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
@@ -1,4 +1,5 @@
 package mate.academy.spring.security;
+
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
 import org.springframework.security.core.userdetails.UserDetails;
