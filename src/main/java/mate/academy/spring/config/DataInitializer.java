bdefccbe63d30a99a79d5523ec3b473d6a8ccc0f
dvdovynets <vdovinec@gmail.com>
2021-07-02 20:00:22 +0300

    used @PostConstruct

--- src/main/java/mate/academy/spring/config/DataInitializer.java
+++ src/main/java/mate/academy/spring/config/DataInitializer.java
@@ -1,16 +1,14 @@
-package mate.academy.spring.controller;
+package mate.academy.spring.config;
 
 import java.util.Set;
+import javax.annotation.PostConstruct;
 import mate.academy.spring.model.Role;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.RoleService;
 import mate.academy.spring.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Component;
 
-@RestController
-@RequestMapping("/inject")
+@Component
 public class DataInitializer {
     private final RoleService roleService;
     private final UserService userService;
@@ -20,7 +18,18 @@ public class DataInitializer {
         this.userService = userService;
     }
 
-    @GetMapping("/users")
+    @PostConstruct
+    public String injectRoles() {
+        Role adminRole = new Role();
+        adminRole.setName(Role.RoleName.ADMIN);
+        Role userRole = new Role();
+        userRole.setName(Role.RoleName.USER);
+        roleService.add(adminRole);
+        roleService.add(userRole);
+        return "Roles were added successfully!";
+    }
+
+    @PostConstruct
     public String injectUsers() {
         User bob = new User();
         bob.setEmail("bob@gmail.com");
@@ -35,16 +44,5 @@ public class DataInitializer {
         alice.setRoles(Set.of(userRole));
         userService.add(alice);
         return "Users were added successfully!";
-    }
-
-    @GetMapping("/roles")
-    public String injectRoles() {
-        Role adminRole = new Role();
-        adminRole.setName(Role.RoleName.ADMIN);
-        Role userRole = new Role();
-        userRole.setName(Role.RoleName.USER);
-        roleService.add(adminRole);
-        roleService.add(userRole);
-        return "Roles were added successfully!";
     }
 }
--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/register", "/inject/roles", "/inject/users").permitAll()
+                .antMatchers("/register").permitAll()
                 .antMatchers(HttpMethod.GET, "/movies", "/cinema-halls",
                         "/movie-sessions/available").hasAnyRole("ADMIN", "USER")
                 .antMatchers(HttpMethod.GET, "/shopping-carts/by-user/*",
