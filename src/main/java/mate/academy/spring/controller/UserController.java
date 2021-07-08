858c6d1c804f429da7597cfabf3308730c154de8
dvdovynets <vdovinec@gmail.com>
2021-07-08 12:52:57 +0300

    made changes in UserDetailService

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -1,11 +1,9 @@
 package mate.academy.spring.controller;
 
 import mate.academy.spring.dto.response.UserResponseDto;
-import mate.academy.spring.exception.DataProcessingException;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
 import mate.academy.spring.service.mapper.UserMapper;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,12 +22,7 @@ public class UserController {
 
     @GetMapping("/by-email")
     public UserResponseDto getByEmail(@RequestParam String email) {
-        User user = null;
-        try {
-            user = userService.findByEmail(email);
-        } catch (DataProcessingException e) {
-            throw new UsernameNotFoundException("Can't find user by email=" + email, e);
-        }
+        User user = userService.findByEmail(email);
         return userMapper.mapToDto(user);
     }
 }
--- src/main/java/mate/academy/spring/security/CustomUserDetailService.java
+++ src/main/java/mate/academy/spring/security/CustomUserDetailService.java
@@ -3,6 +3,7 @@ package mate.academy.spring.security;
 import static org.springframework.security.core.userdetails.User.UserBuilder;
 import static org.springframework.security.core.userdetails.User.withUsername;
 
+import mate.academy.spring.exception.DataProcessingException;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,7 +22,12 @@ public class CustomUserDetailService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username)
             throws UsernameNotFoundException {
-        User user = userService.findByEmail(username);
+        User user = null;
+        try {
+            user = userService.findByEmail(username);
+        } catch (DataProcessingException e) {
+            throw new UsernameNotFoundException("Can't find user by email=" + username, e);
+        }
         UserBuilder builder = withUsername(username);
         builder.password(user.getPassword());
         builder.roles(user.getRoles()
