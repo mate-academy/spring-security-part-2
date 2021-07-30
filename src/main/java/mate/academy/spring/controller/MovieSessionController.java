642d57938a9cf6de8e4f119a6dd8bee455cb825e
Максим <mbelmasg@gmail.com>
2021-07-30 15:18:07 +0300

    fixed

--- src/main/java/mate/academy/spring/controller/MovieSessionController.java
+++ src/main/java/mate/academy/spring/controller/MovieSessionController.java
@@ -3,7 +3,6 @@ package mate.academy.spring.controller;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
-import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 import mate.academy.spring.dto.request.MovieSessionRequestDto;
 import mate.academy.spring.dto.response.MovieSessionResponseDto;
