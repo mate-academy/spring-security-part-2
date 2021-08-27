13840fc47b1addc17f67968a2d0cfc2b1bd20fdd
Basilius the Third <champfockes@gmail.com>
2021-08-27 16:35:56 +0300

    added solution for spring security part 2

---
+++ src/main/java/mate/academy/spring/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package mate.academy.spring.service;
+
+import mate.academy.spring.model.Role;
+import mate.academy.spring.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByEmail(username).orElseThrow(
+                () -> new UsernameNotFoundException("User not found by username " + username));
+        org.springframework.security.core.userdetails.User.UserBuilder userBuilder =
+                org.springframework.security.core.userdetails.User.withUsername(username);
+        userBuilder.password(user.getPassword());
+        userBuilder.roles(user.getRoles().stream()
+                .map(r -> r.getRoleName().name())
+                .toArray(String[]::new));
+        return userBuilder.build();
+    }
+}
