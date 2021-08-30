7a13d18ea6a6c55dc5f3b0cba851e620c2e48e1b
Vova Bilodon <vovabilodon@gmail.com>
2021-08-30 14:31:16 +0300

    checkstyle fix

--- src/main/java/mate/academy/spring/util/DataInitializer.java
+++ src/main/java/mate/academy/spring/util/DataInitializer.java
@@ -4,7 +4,6 @@ import java.util.Set;
 import javax.annotation.PostConstruct;
 import mate.academy.spring.model.Role;
 import mate.academy.spring.model.User;
-import mate.academy.spring.service.AuthenticationService;
 import mate.academy.spring.service.RoleService;
 import mate.academy.spring.service.UserService;
 import org.springframework.stereotype.Component;
