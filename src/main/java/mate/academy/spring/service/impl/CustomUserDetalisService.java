98a3dec0bc7aaa89b40527c062dd1edf0f4e42d9
vKutik <84785564+vKutik@users.noreply.github.com>
2021-08-30 14:49:15 +0300

    little change

--- src/main/java/mate/academy/spring/service/impl/CustomUserDetalisService.java
+++ src/main/java/mate/academy/spring/service/impl/CustomUserDetalisService.java
@@ -5,7 +5,6 @@ import static org.springframework.security.core.userdetails.User.withUsername;
 import mate.academy.spring.model.Role;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
