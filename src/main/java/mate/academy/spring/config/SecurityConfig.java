d3323eb4911b3102ae32087d83129fd9adcda7b4
Evgen Krasnenkov <krasnenkov.evgen@gmail.com>
2021-11-05 17:09:44 +0200

    sr v1 working build with DB auth and PostConstruct no typos

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -37,8 +37,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         String.valueOf(ADMIN), String.valueOf(USER))
                 .antMatchers(HttpMethod.PUT, "/shopping-carts/**").hasAnyRole(
                         String.valueOf(ADMIN), String.valueOf(USER))
-                .antMatchers(HttpMethod.GET, "/shopping-carts/**").hasAnyRole(
-                        String.valueOf(ADMIN), String.valueOf(USER))
                 .antMatchers(HttpMethod.GET, "/orders").hasAnyRole(
                         String.valueOf(ADMIN))
                 .antMatchers(HttpMethod.POST, "/orders/complete").hasAnyRole(
