e7fd716975f42d49a0ee0f3853c6f6252da8355d
Solomiia Hrynda <solomiyaaaa@gmail.com>
2021-11-11 00:01:14 +0200

    implemented DB authentication

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -1,27 +1,60 @@
 package mate.academy.spring.config;
 
+import mate.academy.spring.model.RoleName;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .passwordEncoder(getEncoder())
-                .withUser("1@123").password(getEncoder().encode("123")).roles("USER");
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/register").permitAll()
+                .antMatchers(HttpMethod.GET, "/cinema-halls").hasAnyAuthority(
+                        RoleName.ADMIN.name(), RoleName.USER.name())
+                .antMatchers(HttpMethod.POST, "/cinema-halls").hasAuthority(
+                        RoleName.ADMIN.name())
+                .antMatchers(HttpMethod.GET, "/movies").hasAnyAuthority(
+                        RoleName.ADMIN.name(), RoleName.USER.name())
+                .antMatchers(HttpMethod.POST, "/movies").hasAuthority(
+                        RoleName.ADMIN.name())
+                .antMatchers("/movie-sessions/*").hasAnyAuthority(
+                        RoleName.ADMIN.name(), RoleName.USER.name())
+                .antMatchers(HttpMethod.POST, "/movie-sessions").hasAuthority(
+                        RoleName.ADMIN.name())
+                .antMatchers(HttpMethod.PUT, "/movie-sessions/{id}").hasAuthority(
+                        RoleName.ADMIN.name())
+                .antMatchers(HttpMethod.DELETE, "/movie-sessions/{id}").hasAuthority(
+                        RoleName.ADMIN.name())
+                .antMatchers("/orders").hasAuthority(
+                        RoleName.USER.name())
+                .antMatchers(HttpMethod.POST, "/orders/complete").hasAuthority(
+                        RoleName.USER.name())
+                .antMatchers("/shopping-carts/movie-sessions").hasAuthority(
+                        RoleName.USER.name())
+                .antMatchers("/shopping-carts/by-user").hasAuthority(
+                        RoleName.USER.name())
+                .antMatchers("/users/by-email").hasAuthority(
+                        RoleName.ADMIN.name())
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -30,10 +63,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .csrf().disable();
-    }
-
-    @Bean
-    public PasswordEncoder getEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/mate/academy/spring/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/AuthenticationServiceImpl.java
@@ -1,5 +1,8 @@
 package mate.academy.spring.service.impl;
 
+import java.util.HashSet;
+import mate.academy.spring.model.Role;
+import mate.academy.spring.model.RoleName;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.AuthenticationService;
 import mate.academy.spring.service.ShoppingCartService;
@@ -10,11 +13,14 @@ import org.springframework.stereotype.Service;
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserService userService;
     private final ShoppingCartService shoppingCartService;
+    private final RoleServiceImpl roleService;
 
     public AuthenticationServiceImpl(UserService userService,
-                                     ShoppingCartService shoppingCartService) {
+                                     ShoppingCartService shoppingCartService,
+                                     RoleServiceImpl roleService) {
         this.userService = userService;
         this.shoppingCartService = shoppingCartService;
+        this.roleService = roleService;
     }
 
     @Override
@@ -22,6 +28,9 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         User user = new User();
         user.setEmail(email);
         user.setPassword(password);
+        user.setRoles(new HashSet<>());
+        Role role = roleService.getRoleByName(RoleName.USER);
+        user.getRoles().add(role);
         userService.add(user);
         shoppingCartService.registerNewShoppingCart(user);
         return user;
