d05b7f5b099856f26c22009b153768cd60de517d
P.Zbarazkyy <mtu09@ukr.net>
2021-11-18 08:30:30 +0200

    Added role entity, dao and service, configured DB authentication

--- src/main/java/mate/academy/spring/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/AuthenticationServiceImpl.java
@@ -1,7 +1,10 @@
 package mate.academy.spring.service.impl;
 
+import java.util.HashSet;
+import mate.academy.spring.model.RoleName;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.AuthenticationService;
+import mate.academy.spring.service.RoleService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
 import org.springframework.stereotype.Service;
@@ -10,11 +13,14 @@ import org.springframework.stereotype.Service;
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserService userService;
     private final ShoppingCartService shoppingCartService;
+    private final RoleService roleService;
 
     public AuthenticationServiceImpl(UserService userService,
-                                     ShoppingCartService shoppingCartService) {
+                                     ShoppingCartService shoppingCartService,
+                                     RoleService roleService) {
         this.userService = userService;
         this.shoppingCartService = shoppingCartService;
+        this.roleService = roleService;
     }
 
     @Override
@@ -22,6 +28,8 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         User user = new User();
         user.setEmail(email);
         user.setPassword(password);
+        user.setRoles(new HashSet<>());
+        user.getRoles().add(roleService.getRoleByName(RoleName.USER.toString()));
         userService.add(user);
         shoppingCartService.registerNewShoppingCart(user);
         return user;
