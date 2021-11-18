a4d8bead548d1860d6616399af6f344c92a99355
Stas Shepet <milka33349@gmail.com>
2021-11-19 00:37:55 +0200

    roles and permits was implemented

---
+++ src/main/java/mate/academy/spring/config/DataInitializer.java
@@ -0,0 +1,42 @@
+package mate.academy.spring.config;
+
+import java.util.Set;
+import javax.annotation.PostConstruct;
+import mate.academy.spring.model.Role;
+import mate.academy.spring.model.RoleName;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.RoleService;
+import mate.academy.spring.service.UserService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer {
+    private final RoleService roleService;
+    private final UserService userService;
+
+    public DataInitializer(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    @PostConstruct
+    public void inject() {
+        Role adminRole = new Role();
+        adminRole.setRoleName(RoleName.ADMIN);
+        roleService.add(adminRole);
+        Role userRole = new Role();
+        userRole.setRoleName(RoleName.USER);
+        roleService.add(userRole);
+        User user = new User();
+        user.setEmail("anna");
+        user.setPassword("1234");
+        user.setRoles(Set.of(adminRole));
+        userService.add(user);
+        User user2 = new User();
+        user2.setEmail("bohdan");
+        user2.setPassword("4321");
+        user2.setRoles(Set.of(userRole));
+        userService.add(user2);
+
+    }
+}
