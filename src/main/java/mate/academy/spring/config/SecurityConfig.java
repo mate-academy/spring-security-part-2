ef34209ace22452ff2bd7316513bdee474df1664
P.Zbarazkyy <mtu09@ukr.net>
2021-11-18 09:49:20 +0200

    Added role entity, dao and service, configured DB authentication

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -27,18 +27,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers(HttpMethod.POST,"/register").permitAll()
-                .antMatchers(HttpMethod.GET,"/cinema-halls", "/movies",
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/register").permitAll()
+                .antMatchers(HttpMethod.GET, "/cinema-halls", "/movies",
                         "/movie-sessions/available", "/movie-sessions/**")
-                .hasAnyRole("ADMIN","USER")
+                .hasAnyRole("ADMIN", "USER")
                 .antMatchers(HttpMethod.GET, "/orders", "/shopping-carts/by-user")
                 .hasAnyRole("USER")
                 .antMatchers(HttpMethod.GET, "/users/by-email").hasAnyRole("ADMIN")
                 .antMatchers(HttpMethod.POST, "/cinema-halls",
                         "/movies", "/movie-sessions").hasAnyRole("ADMIN")
-                .antMatchers(HttpMethod.POST,"/orders/complete").hasAnyRole("USER")
+                .antMatchers(HttpMethod.POST, "/orders/complete").hasAnyRole("USER")
                 .antMatchers(HttpMethod.PUT, "/movie-sessions/**").hasAnyRole("ADMIN")
                 .antMatchers(HttpMethod.PUT, "/shopping-carts/movie-sessions").hasAnyRole("USER")
                 .antMatchers(HttpMethod.DELETE, "/movie-sessions/**").hasAnyRole("ADMIN")
