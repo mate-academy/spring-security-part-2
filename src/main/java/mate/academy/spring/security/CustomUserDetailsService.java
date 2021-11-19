ee53e7426f0fa1f3722671306788bf5d3657b1da
V <v.ivashchenko42@protonmail.com>
2021-11-19 09:51:41 +0300

    configured DB authentication, added Role entity

---
+++ src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package mate.academy.spring.security;
+
+import java.util.Optional;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userService.findByEmail(email);
+        if (optionalUser.isEmpty()) {
+            throw new UsernameNotFoundException("User not found " + email);
+        }
+        User user = optionalUser.get();
+        UserBuilder userBuilder =
+                org.springframework.security.core.userdetails.User.withUsername(email);
+        userBuilder.password(user.getPassword());
+        userBuilder.authorities(user.getRoles().stream()
+                .map(r -> r.getRoleType().name())
+                .toArray(String[]::new));
+        return userBuilder.build();
+    }
+}
