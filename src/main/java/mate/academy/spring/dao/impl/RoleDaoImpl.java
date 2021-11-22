2aea7e06f08c85099860590ab536d6713eb9372f
SolnalarmJulia <solnalarm@meta.ua>
2021-11-22 12:54:29 +0200

    refactoring RoleDao add()

--- src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
@@ -1,42 +1,19 @@
 package mate.academy.spring.dao.impl;
 
 import java.util.Optional;
+import mate.academy.spring.dao.AbstractDao;
 import mate.academy.spring.dao.RoleDao;
 import mate.academy.spring.exception.DataProcessingException;
 import mate.academy.spring.model.Role;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class RoleDaoImpl implements RoleDao {
-    private final SessionFactory factory;
-
+public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
     public RoleDaoImpl(SessionFactory factory) {
-        this.factory = factory;
-    }
-
-    @Override
-    public void add(Role role) {
-        Transaction transaction = null;
-        Session session = null;
-        try {
-            session = factory.openSession();
-            transaction = session.beginTransaction();
-            session.save(role);
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            throw new DataProcessingException("Can't insert role " + role, e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        super(factory, Role.class);
     }
 
     @Override
@@ -47,7 +24,7 @@ public class RoleDaoImpl implements RoleDao {
             findByRoleName.setParameter("roleName", Role.RoleName.valueOf(roleName));
             return findByRoleName.uniqueResultOptional();
         } catch (Exception e) {
-            throw new DataProcessingException("Role with roleName " + roleName + " not found", e);
+            throw new DataProcessingException("Can't get Role by RoleName" + roleName, e);
         }
     }
 }
