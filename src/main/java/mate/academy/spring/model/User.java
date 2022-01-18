a1370715d283da3428211c16b158fc2d4a91c3e2
Olha Tankevych <freakystyley85@gmail.com>
2022-01-18 16:08:32 +0200

    renamed Columns in JoinTable

--- src/main/java/mate/academy/spring/model/User.java
+++ src/main/java/mate/academy/spring/model/User.java
@@ -22,8 +22,8 @@ public class User {
     private String password;
     @ManyToMany
     @JoinTable(name = "users_roles",
-            joinColumns = @JoinColumn(name = "users"),
-            inverseJoinColumns = @JoinColumn(name = "roles"))
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
     public Set<Role> getRoles() {
