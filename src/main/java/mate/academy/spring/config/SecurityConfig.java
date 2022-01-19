00fd39f9bdd167273c3dfa6027bee3031a502dc5
Mykhailo Tereshchenkov <mtwib.re@gmail.com>
2022-01-19 11:48:33 +0200

    Initial commit

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package mate.academy.spring.config;
 
+import lombok.AllArgsConstructor;
+import mate.academy.spring.model.Role;
+import mate.academy.spring.security.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,24 +14,38 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final String ROLE_ADMIN = Role.RoleName.ADMIN.name();
+    private static final String ROLE_USER = Role.RoleName.USER.name();
+    private final PasswordEncoder passwordEncoder;
+    private final CustomUserDetailsService userDetailsService;
+
     @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .passwordEncoder(getEncoder())
-                .withUser("1@123").password(getEncoder().encode("123")).roles("USER");
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/cinema-halls",
+                        "/movies",
+                        "/movie-sessions/{id}",
+                        "/movie-sessions/available").hasAnyRole(ROLE_ADMIN, ROLE_USER)
+                .antMatchers(HttpMethod.GET, "/orders",
+                        "/shopping-carts/by-user").hasRole(ROLE_USER)
+                .antMatchers(HttpMethod.GET, "/users/by-email").hasRole(ROLE_ADMIN)
+                .antMatchers(HttpMethod.POST, "/register").permitAll()
+                .antMatchers(HttpMethod.POST, "/orders/complete").hasRole(ROLE_USER)
+                .antMatchers(HttpMethod.POST, "/cinema-halls",
+                        "/movies", "/movie-sessions").hasRole(ROLE_ADMIN)
+                .antMatchers(HttpMethod.PUT, "/shopping-carts/movie-session").hasRole(ROLE_USER)
+                .antMatchers(HttpMethod.PUT, "/movie-sessions/{id}").hasRole(ROLE_ADMIN)
+                .antMatchers(HttpMethod.DELETE, "/movie-sessions/{id}").hasRole(ROLE_ADMIN)
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .permitAll()
-                .and()
-                .httpBasic()
                 .and()
                 .csrf().disable();
     }
