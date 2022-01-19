957744a538b475cd47e694e0124bf938165fd05b
Yevheniy Savenkov <yevheniy.savenkov@gmail.com>
2022-01-19 16:26:48 +0200

    one more underscore

--- src/main/java/mate/academy/spring/model/ShoppingCart.java
+++ src/main/java/mate/academy/spring/model/ShoppingCart.java
@@ -11,7 +11,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "shoppingcarts")
+@Table(name = "shopping_carts")
 public class ShoppingCart {
     @Id
     private Long id;
