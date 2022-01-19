44908a4ff4036b004ec8843e9e017c071ede4214
Dzmitry Kozel <dmitrokoz88@gmail.com>
2022-01-19 09:54:29 +0200

    roles fixed;

--- src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
+++ src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
@@ -21,7 +21,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         UserBuilder builder
                 = org.springframework.security.core.userdetails.User.withUsername(username);
         builder.password(user.getPassword());
-        builder.authorities(user.getRoles().stream()
+        builder.roles(user.getRoles().stream()
                 .map(r -> r.getRoleName().name()).toArray(String[]::new));
         return builder.build();
     }
