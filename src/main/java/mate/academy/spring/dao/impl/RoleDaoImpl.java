55a94a237d8b1be891d03a316d4a38157b5cbd3a
Evgen Lanovenko <ev.lanovenko@gmail.com>
2022-03-22 18:50:17 +0200

    realized cinema app

---
+++ src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
@@ -0,0 +1,29 @@
+package mate.academy.spring.dao.impl;
+
+import java.util.Optional;
+import mate.academy.spring.dao.AbstractDao;
+import mate.academy.spring.dao.RoleDao;
+import mate.academy.spring.exception.DataProcessingException;
+import mate.academy.spring.model.Role;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
+    public RoleDaoImpl(SessionFactory factory) {
+        super(factory, Role.class);
+    }
+
+    @Override
+    public Optional<Role> getRoleByName(String roleName) {
+        try (Session session = factory.openSession()) {
+            Query<Role> query = session.createQuery("FROM Role WHERE name = :name", Role.class);
+            query.setParameter("name", roleName);
+            return query.uniqueResultOptional();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find role " + roleName, e);
+        }
+    }
+}
