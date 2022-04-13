d93c66866e0f070aa89fe4ac60761680ba5483c1
sviat-h <svyatoslav.grinik@gmail.com>
2022-04-13 19:08:13 +0300

    DB authentication was implemented

---
+++ src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package mate.academy.spring.security;
+
+import java.util.Optional;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userService.findByEmail(email);
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("User not found!");
+        }
+        org.springframework.security.core.userdetails.User.UserBuilder builder =
+                org.springframework.security.core.userdetails.User.withUsername(email);
+        builder.password(user.get().getPassword());
+        builder.roles(user.get().getRoles()
+                .stream()
+                .map(r -> r.getRoleName().name())
+                .toArray(String[]::new));
+        return builder.build();
+    }
+}
