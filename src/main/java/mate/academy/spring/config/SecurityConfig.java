e86fea7efca27b64332dd6ee34e4d0986cc24327
AlexPravda1 <kabinkos@gmail.com>
2022-04-17 17:19:34 +0400

    - Configured DB authentication - Added Role entity, Dao and Service layer - Configured role access to specific resources

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -1,28 +1,65 @@
 package mate.academy.spring.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .passwordEncoder(getEncoder())
-                .withUser("1@123").password(getEncoder().encode("123")).roles("USER");
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
     }
 
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    /*
+GET: /orders - user
+POST: /orders/complete - user
+PUT: /shopping-carts/movie-sessions - user
+GET: /shopping-carts/by-user - user
+GET: /users/by-email - admin
+     */
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated()
+
+                .antMatchers(HttpMethod.POST,
+                        "/movies",
+                        "/cinema-halls",
+                        "/movie-sessions").hasRole("ADMIN")
+
+                .antMatchers(HttpMethod.PUT,
+                        "/movie-sessions/{id}").hasRole("ADMIN")
+
+                .antMatchers(HttpMethod.DELETE,
+                        "/movie-sessions/{id}").hasRole("ADMIN")
+
+                .antMatchers(HttpMethod.GET,
+                        "/shopping-carts",
+                        "/orders/**").hasRole("USER")
+
+                .antMatchers(HttpMethod.GET,
+                        "/users/**").hasRole("ADMIN")
+
+                .antMatchers(HttpMethod.GET,
+                        "/register/**",
+                        "/movies/**",
+                        "/cinema-halls/**",
+                        "/movie-sessions/available").permitAll()
+
+                .anyRequest().fullyAuthenticated()
                 .and()
                 .formLogin()
                 .permitAll()
@@ -30,10 +67,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .csrf().disable();
-    }
-
-    @Bean
-    public PasswordEncoder getEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/mate/academy/spring/model/Role.java
@@ -0,0 +1,58 @@
+package mate.academy.spring.model;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(value = EnumType.STRING)
+    @Column(unique = true)
+    private RoleName roleName;
+
+    public enum RoleName {
+        ADMIN,
+        USER,
+        MODERATOR
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+            return false;
+        }
+        Role role = (Role) o;
+        return id != null && Objects.equals(id, role.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Role{"
+                + "id=" + id
+                + ", roleName=" + roleName.toString()
+                + '}';
+    }
+}
