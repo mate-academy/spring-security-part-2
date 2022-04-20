06df9392bb62bb8eaef123e0f5e647252b6a9256
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-04-20 14:57:19 +0300

    Update SecurityConfig.configure(HttpSecurity http)

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package mate.academy.spring.config;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,6 +28,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/register")
+                .permitAll()
+                .antMatchers(HttpMethod.GET,
+                        "/cinema-halls",
+                        "/movies",
+                        "/movie-sessions/available")
+                .hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.POST,
+                        "/cinema-halls",
+                        "/movies",
+                        "/movie-sessions")
+                .hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/movie-sessions/**")
+                .hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/movie-sessions/**")
+                .hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/users/by-email")
+                .hasRole("ADMIN")
+                .antMatchers("/orders")
+                .hasRole("USER")
+                .antMatchers("/shopping-carts/**")
+                .hasRole("USER")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
