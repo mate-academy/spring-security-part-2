49515caccfb98431a3d796e0ba9f853786a1bc37
Nazarii Yatsiuk <nazariiyatsiuk@gmail.com>
2022-05-01 22:11:57 +0300

    First commit

---
+++ src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
@@ -0,0 +1,28 @@
+package mate.academy.spring.dao.impl;
+
+import mate.academy.spring.dao.AbstractDao;
+import mate.academy.spring.dao.RoleDao;
+import mate.academy.spring.exception.DataProcessingException;
+import mate.academy.spring.model.Role;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
+    public RoleDaoImpl(SessionFactory factory) {
+        super(factory, Role.class);
+    }
+
+    @Override
+    public Role getByName(Role.RoleName roleName) {
+        try (Session session = factory.openSession()) {
+            Query<Role> query = session.createQuery("FROM Role r where name = :rolename");
+            query.setParameter("rolename", roleName);
+            return query.getSingleResult();
+        } catch (Exception e) {
+            throw new DataProcessingException("Role by name " + roleName + " not found", e);
+        }
+    }
+}
