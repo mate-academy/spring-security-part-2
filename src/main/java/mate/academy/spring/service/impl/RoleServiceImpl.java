b9dca8321997787fcbd96e300d22ff4b365d7880
sltnsrh <lifenjoy8888@gmail.com>
2022-05-02 09:27:55 +0300

    Role entity, dao, service, and enum RoleName were created

---
+++ src/main/java/mate/academy/spring/service/impl/RoleServiceImpl.java
@@ -0,0 +1,18 @@
+package mate.academy.spring.service.impl;
+
+import mate.academy.spring.model.Role;
+import mate.academy.spring.service.RoleService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+    @Override
+    public Role add(Role role) {
+        return null;
+    }
+
+    @Override
+    public Role getByName(String roleName) {
+        return null;
+    }
+}
