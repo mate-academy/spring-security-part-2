5457daa663a2a5b78f8d18419eb53aa9bb9c5ff9
Anton Ignatenko <87337009+antonignatenko@users.noreply.github.com>
2022-05-06 15:45:37 +0300

    deleted annotation

--- src/main/java/mate/academy/spring/service/impl/RoleServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/RoleServiceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class RoleServiceImpl implements RoleService {
-    @Autowired
     private final RoleDao roleDao;
 
     public RoleServiceImpl(RoleDao roleDao) {
