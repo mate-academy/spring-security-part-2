0a5594f1c3ee36da69cda03a7889bfc71f01d3ed
Tatka <tlogatskaya96@gmail.com>
2022-05-11 17:09:00 +0300

    implemented spring security part 2

---
+++ src/main/java/mate/academy/spring/config/DataInitializer.java
@@ -0,0 +1,41 @@
+package mate.academy.spring.config;
+
+import mate.academy.spring.model.Role;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.RoleService;
+import mate.academy.spring.service.UserService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Set;
+
+@Component
+public class DataInitializer {
+    private final RoleService roleService;
+    private final UserService userService;
+
+    public DataInitializer(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    @PostConstruct
+    public void inject() {
+        Role adminRole = new Role();
+        adminRole.setRoleName(Role.RoleName.ADMIN);
+        roleService.add(adminRole);
+        Role userRole = new Role();
+        userRole.setRoleName(Role.RoleName.USER);
+        roleService.add(userRole);
+        User user = new User();
+        user.setEmail("admin@i.ua");
+        user.setPassword("admin123");
+        user.setRoles(Set.of(adminRole));
+        userService.add(user);
+        User user1 = new User();
+        user1.setEmail("user@i.ua");
+        user1.setPassword("user123");
+        user1.setRoles(Set.of(userRole));
+        userService.add(user1);
+    }
+}
