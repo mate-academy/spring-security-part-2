0c8b6352929f7ad326c1df921b8d7547d74cf224
Maksym Pokras <mxpokras@gmail.com>
2022-06-04 12:45:04 +0200

    roles added to User

--- src/main/java/mate/academy/spring/model/User.java
+++ src/main/java/mate/academy/spring/model/User.java
@@ -5,7 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
@@ -16,6 +18,8 @@ public class User {
     @Column(unique = true)
     private String email;
     private String password;
+    @ManyToMany
+    Set<Role> roles;
 
     public Long getId() {
         return id;
@@ -41,10 +45,19 @@ public class User {
         this.password = password;
     }
 
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
     @Override
     public String toString() {
         return "User{"
                 + "id=" + id
-                + ", email='" + email + '\'' + '}';
+                + ", email='" + email + '\''
+                + ", roles=" + roles + '}';
     }
 }
