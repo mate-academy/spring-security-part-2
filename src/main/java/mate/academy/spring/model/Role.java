d1d3e9781d695f4f49a624b5c7fb3f6e6db6bbfd
Maksym Pokras <mxpokras@gmail.com>
2022-06-04 12:37:34 +0200

    Role entity created

---
+++ src/main/java/mate/academy/spring/model/Role.java
@@ -0,0 +1,43 @@
+package mate.academy.spring.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column(unique = true)
+    RoleName roleName;
+
+    public enum RoleName {
+        ADMIN,
+        USER;
+
+        public String getRoleName() {
+            return "ROLE_" + name();
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
+}
