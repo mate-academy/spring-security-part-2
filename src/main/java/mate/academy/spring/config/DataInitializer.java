a4c084e4f2fc646df216ff8b025a1f078c022673
Maksym Pokras <mxpokras@gmail.com>
2022-06-04 15:06:39 +0200

    DataInitializer bean created

---
+++ src/main/java/mate/academy/spring/config/DataInitializer.java
@@ -0,0 +1,35 @@
+package mate.academy.spring.config;
+
+import mate.academy.spring.model.Role;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.RoleService;
+import mate.academy.spring.service.UserService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Set;
+
+@Component
+public class DataInitializer {
+    private final RoleService roleService;
+    private final UserService userService;
+
+    public DataInitializer(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    @PostConstruct
+    public void inject() {
+        Role adminRole = new Role();
+        adminRole.setRoleName(Role.RoleName.ADMIN);
+        roleService.save(adminRole);
+        Role userRole = new Role();
+        userRole.setRoleName(Role.RoleName.USER);
+        roleService.save(userRole);
+        User user = new User();
+        user.setEmail("admin@i.ua");
+        user.setPassword("admin123");
+        user.setRoles(Set.of(adminRole));
+        userService.add(user);    }
+}
