42d91343328fabfaf4eeb75fa54c4dd2cffd2809
Maksym Pokras <mxpokras@gmail.com>
2022-06-05 11:21:58 +0200

    roleName field annotated as enumerated

--- src/main/java/mate/academy/spring/model/Role.java
+++ src/main/java/mate/academy/spring/model/Role.java
@@ -2,6 +2,8 @@ package mate.academy.spring.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -14,6 +16,7 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     @Column(unique = true)
+    @Enumerated(value = EnumType.STRING)
     RoleName roleName;
 
     public enum RoleName {
