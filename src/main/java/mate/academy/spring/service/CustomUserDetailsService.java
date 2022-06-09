176ff4e048f13740d1d3790e0def8d484e79078f
Mark Shapka <mark.shapka1@gmail.com>
2022-06-09 14:07:43 +0300

    task spring-security-part-2 - added entity Role, implemented dao and service layer - implemented CustomUserDetailsService - added DataInitializer class with inject() method - added access according roles for endpoints

---
+++ src/main/java/mate/academy/spring/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package mate.academy.spring.service;
+
+import java.util.Optional;
+import mate.academy.spring.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userFromDB = userService.findByEmail(username);
+        if (userFromDB.isEmpty()) {
+            throw new UsernameNotFoundException("Can't find user by username: " + username);
+        }
+        org.springframework.security.core.userdetails.User.UserBuilder builder =
+                org.springframework.security.core.userdetails.User.withUsername(username);
+        builder.password(userFromDB.get().getPassword());
+        builder.authorities(userFromDB.get().getRoles().stream()
+                .map(r -> "ROLE_" + r.getRoleName().name())
+                .toArray(String[]::new));
+        return builder.build();
+    }
+}
