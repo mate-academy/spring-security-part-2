cd1a3e61c1678583b5026ef346cf95e0d2ec99ab
Mark Shapka <mark.shapka1@gmail.com>
2022-06-09 14:48:03 +0300

    - deleted annotation @Autowired and added constructor

--- src/main/java/mate/academy/spring/service/CustomUserDetailsService.java
+++ src/main/java/mate/academy/spring/service/CustomUserDetailsService.java
@@ -2,7 +2,6 @@ package mate.academy.spring.service;
 
 import java.util.Optional;
 import mate.academy.spring.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,8 +9,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-    @Autowired
     private UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
