eb3f16f12bb2816622e291298405f1a1eb92d763
Dmytro Koliada <dmitrikstar@gmail.com>
2022-06-23 09:24:56 +0300

    added role autorization

--- src/main/java/mate/academy/spring/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/AuthenticationServiceImpl.java
@@ -1,7 +1,11 @@
 package mate.academy.spring.service.impl;
 
+import java.util.HashSet;
+import java.util.Set;
+import mate.academy.spring.model.Role;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.AuthenticationService;
+import mate.academy.spring.service.RoleService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
 import org.springframework.stereotype.Service;
@@ -10,16 +14,22 @@ import org.springframework.stereotype.Service;
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserService userService;
     private final ShoppingCartService shoppingCartService;
+    private final RoleService roleService;
 
     public AuthenticationServiceImpl(UserService userService,
-                                     ShoppingCartService shoppingCartService) {
+                                     ShoppingCartService shoppingCartService,
+                                     RoleService roleService) {
         this.userService = userService;
         this.shoppingCartService = shoppingCartService;
+        this.roleService = roleService;
     }
 
     @Override
     public User register(String email, String password) {
         User user = new User();
+        Set<Role> roleSet = new HashSet<>();
+        roleSet.add(roleService.getByName(Role.RoleName.USER.name()));
+        user.setRoles(roleSet);
         user.setEmail(email);
         user.setPassword(password);
         userService.add(user);
