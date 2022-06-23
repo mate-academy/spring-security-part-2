793d0ffd53701e68f1a9b6fac5921984163aa23a
Andrii Synelnyk <sinelnik2154@gmail.com>
2022-06-23 16:15:14 +0300

    this finally works

---
+++ src/main/java/mate/academy/spring/service/impl/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package mate.academy.spring.service.impl;
+
+import static org.springframework.security.core.userdetails.User.withUsername;
+
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        User user = userService.findByEmail(email).orElseThrow(() ->
+                new UsernameNotFoundException("User with email: " + email + "doesn't exist"));
+        UserBuilder builder = withUsername(user.getEmail());
+        builder.password(user.getPassword());
+        builder.authorities(user.getRoles().stream()
+                .map(r -> r.getRoleName().name())
+                .toArray(String[]::new));
+        return builder.build();
+    }
+}
