1edd2a564ad39fdb613f15a39f5e0b6a5d9353ab
GrabenkoVitaliy <grabenkovitaliy7@gmail.com>
2022-06-24 16:46:04 +0300

    hw has been done

---
+++ src/main/java/mate/academy/spring/model/Role.java
@@ -0,0 +1,37 @@
+package mate.academy.spring.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Enumerated(value = EnumType.STRING)
+    private RoleName roleName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
+
+    public enum RoleName {
+        USER,
+        ADMIN
+    }
+}
---
+++ src/main/java/mate/academy/spring/service/impl/CustomUserDetailsServiceImpl.java
@@ -0,0 +1,36 @@
+package mate.academy.spring.service.impl;
+
+import java.util.Optional;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsServiceImpl implements UserDetailsService {
+    private final UserService userService;
+
+    public CustomUserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> userByEmail = userService.findByEmail(email);
+        UserBuilder builder;
+        if (userByEmail.isPresent()) {
+            User user = userByEmail.get();
+            builder = org.springframework.security.core.userdetails.User
+                    .withUsername(user.getEmail());
+            builder.password(user.getPassword());
+            builder.roles(user.getRole().stream()
+                    .map(r -> r.getRoleName().name())
+                    .toArray(String[]::new));
+            return builder.build();
+        }
+        throw new UsernameNotFoundException("Can't get user by " + email);
+    }
+}
