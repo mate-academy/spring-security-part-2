0ea417159c469858be431d1a7cde4e3e8bdbeade
daniel <daniya522@gmail.com>
2022-07-13 23:56:05 +0300

    completed all tasks

---
+++ src/main/java/mate/academy/spring/config/DataInitializer.java
@@ -0,0 +1,36 @@
+package mate.academy.spring.config;
+
+import java.util.Set;
+import javax.annotation.PostConstruct;
+import mate.academy.spring.model.Role;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.RoleService;
+import mate.academy.spring.service.UserService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer {
+    private final UserService userService;
+    private final RoleService roleService;
+
+    public DataInitializer(UserService userService,
+                           RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @PostConstruct
+    public void inject() {
+        Role adminRole = new Role();
+        adminRole.setRoleName(Role.RoleName.ADMIN);
+        roleService.add(adminRole);
+        Role userRole = new Role();
+        userRole.setRoleName(Role.RoleName.USER);
+        roleService.add(userRole);
+        User admin = new User();
+        admin.setEmail("admin@gmail.com");
+        admin.setPassword("HelloMyRoleIsAdmin");
+        admin.setRoles(Set.of(adminRole));
+        userService.add(admin);
+    }
+}
