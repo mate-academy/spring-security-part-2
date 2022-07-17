206dad114a9037cbcaac3e01c56212b619ee646b
anton-karpov <antonkarpov547@gmail.com>
2022-07-17 21:56:43 +0200

    Cinema shop: user roles and DB authentication implemented

---
+++ src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
@@ -0,0 +1,34 @@
+package mate.academy.spring.dao.impl;
+
+import java.util.Optional;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import mate.academy.spring.dao.AbstractDao;
+import mate.academy.spring.model.Role;
+import mate.academy.spring.service.RoleDao;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
+    public RoleDaoImpl(SessionFactory factory) {
+        super(factory, Role.class);
+    }
+
+    @Override
+    public Optional<Role> getByName(String roleName) {
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Role> query = criteriaBuilder.createQuery(Role.class);
+            Role.RoleName role = null;
+            for (Role.RoleName roleName1 : Role.RoleName.values()) {
+                if (roleName1.getRoleName().equals(roleName)) {
+                    role = roleName1;
+                }
+            }
+            query.where(criteriaBuilder.equal(query.from(Role.class), role));
+            return session.createQuery(query).uniqueResultOptional();
+        }
+    }
+}
