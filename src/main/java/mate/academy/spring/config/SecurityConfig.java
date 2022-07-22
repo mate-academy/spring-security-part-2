3e9cbc9c23c3153e2a1024daf8fff787b37c4815
Denys.Sh <7860denis@gmail.com>
2022-07-22 10:01:27 +0300

    is done

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -1,39 +1,55 @@
 package mate.academy.spring.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .passwordEncoder(getEncoder())
-                .withUser("1@123").password(getEncoder().encode("123")).roles("USER");
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http
                 .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/register").permitAll()
+                .antMatchers(HttpMethod.GET, "/inject").permitAll()
+                .antMatchers(HttpMethod.GET,
+                        "/cinema-halls/**",
+                        "/movie-sessions/available",
+                        "/movies/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.POST,
+                        "/cinema-halls/**",
+                        "/movies/**",
+                        "/movie-sessions/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/movie-sessions/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/movie-sessions/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/users/by-email").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/orders/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/orders/complete").hasRole("USER")
+                .antMatchers(HttpMethod.PUT, "/shopping-carts/movie-sessions").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/shopping-carts/by-user").hasRole("USER")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .permitAll()
                 .and()
-                .httpBasic()
-                .and()
-                .csrf().disable();
-    }
-
-    @Bean
-    public PasswordEncoder getEncoder() {
-        return new BCryptPasswordEncoder();
+                .httpBasic();
     }
 }
--- src/main/java/mate/academy/spring/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/AuthenticationServiceImpl.java
@@ -1,27 +1,34 @@
 package mate.academy.spring.service.impl;
 
+import java.util.Set;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.AuthenticationService;
+import mate.academy.spring.service.RoleService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserService userService;
     private final ShoppingCartService shoppingCartService;
+    private final RoleService roleService;
 
     public AuthenticationServiceImpl(UserService userService,
-                                     ShoppingCartService shoppingCartService) {
+                                     ShoppingCartService shoppingCartService,
+                                     RoleService roleService) {
         this.userService = userService;
         this.shoppingCartService = shoppingCartService;
+        this.roleService = roleService;
     }
 
     @Override
     public User register(String email, String password) {
         User user = new User();
         user.setEmail(email);
-        user.setPassword(password);
+        user.setPassword(user.getPassword());
+        user.setRoles(Set.of(roleService.getByName("USER").get()));
         userService.add(user);
         shoppingCartService.registerNewShoppingCart(user);
         return user;
