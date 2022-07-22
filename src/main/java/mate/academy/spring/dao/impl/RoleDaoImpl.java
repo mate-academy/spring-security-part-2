7431f49f9482956ac4ca24c5b572772c27da46e1
Denys.Sh <7860denis@gmail.com>
2022-07-22 13:33:22 +0300

    refactored after review

--- src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
@@ -20,7 +20,7 @@ public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
     public Optional<Role> getByName(String roleName) {
         try (Session session = factory.openSession()) {
             Query<Role> roleQuery
-                    = session.createQuery("FROM Role WHERE role_name = :role", Role.class);
+                    = session.createQuery("FROM Role WHERE roleName = :roleName", Role.class);
             roleQuery.setParameter("roleName", Role.RoleName.valueOf(roleName));
             return roleQuery.uniqueResultOptional();
         } catch (Exception e) {
