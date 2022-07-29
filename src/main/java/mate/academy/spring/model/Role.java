34695e326d74595ea2cc7af8d79fdbdda6438feb
Mykola Dubich <98731880+Sm0kk@users.noreply.github.com>
2022-07-29 14:01:13 +0200

    Ready to pull request

---
+++ src/main/java/mate/academy/spring/model/Role.java
@@ -0,0 +1,57 @@
+package mate.academy.spring.model;
+
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(value = EnumType.STRING)
+    private RoleName roleName;
+
+    public Role() {
+    }
+
+    public Role(RoleName name) {
+        this.roleName = name;
+    }
+
+    public RoleName getName() {
+        return roleName;
+    }
+
+    public void setName(RoleName name) {
+        this.roleName = name;
+    }
+
+    public enum RoleName {
+        ADMIN,
+        USER
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) && roleName == role.roleName;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, roleName);
+    }
+}
