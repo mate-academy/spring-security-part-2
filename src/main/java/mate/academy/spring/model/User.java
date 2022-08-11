4f7d795c4bcc0ae030ea2e527e4b2ab58cf87738
kqlynero <kqlynero@gmail.com>
2022-08-11 16:15:34 +0300

    -fixed mistakes after review

--- src/main/java/mate/academy/spring/model/User.java
+++ src/main/java/mate/academy/spring/model/User.java
@@ -22,7 +22,8 @@ public class User {
     private String password;
     @ManyToMany
     @JoinTable(name = "user_role",
-            inverseJoinColumns = @JoinColumn(name = "user_id"))
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
     public Set<Role> getRoles() {
