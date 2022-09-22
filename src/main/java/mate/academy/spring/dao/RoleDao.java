e5405f6e0310cec8aceef96891642f577d21c8a7
Benatti <ssunica0@gmail.com>
2022-09-22 14:19:50 +0300

    with problem

---
+++ src/main/java/mate/academy/spring/dao/RoleDao.java
@@ -0,0 +1,15 @@
+package mate.academy.spring.dao;
+
+import java.util.List;
+import java.util.Optional;
+import mate.academy.spring.model.Role;
+
+public interface RoleDao {
+    Role add(Role role);
+
+    Optional<Role> getByName(String roleName);
+
+    List<Role> getAll();
+
+    void delete(Long id);
+}
---
+++ src/main/java/mate/academy/spring/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package mate.academy.spring.service.impl;
+
+import java.util.Optional;
+import mate.academy.spring.model.Role;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserService userService;
+
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> userOptional = userService.findByEmail(email);
+        if (userOptional.isEmpty()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        User user = userOptional.get();
+        org.springframework.security.core.userdetails.User.UserBuilder userBuilder =
+                org.springframework.security.core.userdetails.User.withUsername(email);
+        userBuilder.password(user.getPassword());
+        userBuilder.authorities(user.getRoles().stream()
+                .map(Role::getName).toArray(String[]::new));
+        return userBuilder.build();
+    }
+}
