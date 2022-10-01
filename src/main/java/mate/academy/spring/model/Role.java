f4e4efaf8d2f40d99efc84a9b47f27aa4e5e2080
Nazar <litvik.12@gmail.com>
2022-10-01 19:50:35 +0300

    added realization with roles for users

---
+++ src/main/java/mate/academy/spring/model/Role.java
@@ -0,0 +1,40 @@
+package mate.academy.spring.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(value = EnumType.STRING)
+    private RoleName role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleName getRole() {
+        return role;
+    }
+
+    public void setRole(RoleName role) {
+        this.role = role;
+    }
+
+    public enum RoleName {
+        ADMIN,
+        USER
+    }
+}
