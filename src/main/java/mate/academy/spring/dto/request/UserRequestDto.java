f1029aec25168faf038a286aa02d7c0cabc45aa8
Lyudmila D <dunda94lyudmila@gmail.com>
2022-10-02 16:38:18 +0300

    need advice to fix mistakes. I can't run the project

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,5 +1,6 @@
 package mate.academy.spring.dto.request;
 
+import java.util.List;
 import javax.validation.constraints.Size;
 import mate.academy.spring.lib.FieldsValueMatch;
 import mate.academy.spring.lib.ValidEmail;
@@ -15,6 +16,11 @@ public class UserRequestDto {
     @Size(min = 8, max = 40)
     private String password;
     private String repeatPassword;
+    private List<Long> roleIds;
+
+    public List<Long> getRoleIds() {
+        return roleIds;
+    }
 
     public String getEmail() {
         return email;
