490ceacf8113df956fa99afaa9e47c82babf6ad6
Nazar <litvik.12@gmail.com>
2022-10-03 19:24:55 +0300

    fix users acces

--- src/main/java/mate/academy/spring/service/impl/CustomUserDetailsService.java
+++ src/main/java/mate/academy/spring/service/impl/CustomUserDetailsService.java
@@ -23,7 +23,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         org.springframework.security.core.userdetails.User.UserBuilder builder =
                 org.springframework.security.core.userdetails.User.withUsername(username);
         builder.password(user.getPassword());
-        builder.authorities(user.getRoles().stream()
+        builder.roles(user.getRoles().stream()
                 .map(r -> r.getRole().name()).toArray(String[]::new));
         return builder.build();
     }
