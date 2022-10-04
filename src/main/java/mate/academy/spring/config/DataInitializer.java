892f01128372444512d13c9cb6a4f19f2d6cb8da
Alex Alexandrov <alexandrov.alex.al@gmail.com>
2022-10-04 22:55:51 +0300

    Given task conditions are met.

---
+++ src/main/java/mate/academy/spring/config/DataInitializer.java
@@ -0,0 +1,35 @@
+package mate.academy.spring.config;
+
+import java.util.Set;
+import javax.annotation.PostConstruct;
+import mate.academy.spring.model.Role;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.RoleService;
+import mate.academy.spring.service.UserService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer {
+    private final RoleService roleService;
+    private final UserService userService;
+
+    public DataInitializer(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    @PostConstruct
+    public void inject() {
+        Role admin = new Role();
+        admin.setName(Role.RoleName.ADMIN);
+        roleService.add(admin);
+        Role user = new Role();
+        user.setName(Role.RoleName.USER);
+        roleService.add(user);
+        User adminUser = new User();
+        adminUser.setEmail("admin@email");
+        adminUser.setPassword("admin");
+        adminUser.setRoles(Set.of(admin));
+        userService.add(adminUser);
+    }
+}
