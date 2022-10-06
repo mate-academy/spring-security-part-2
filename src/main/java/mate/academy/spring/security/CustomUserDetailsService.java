11a2d3edcde87a5db6fede82d43aa87bd4e21b74
Iryna Khrustalova <blackkitty2204@gmail.com>
2022-10-06 15:10:18 +0200

    CustomUserDetailsService-corrected-UserBuilder-import-added

--- src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
+++ src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package mate.academy.spring.security;
 import java.util.Optional;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -19,7 +20,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) {
         Optional<User> userFromDb = userService.findByEmail(email);
-        org.springframework.security.core.userdetails.User.UserBuilder userBuilder;
+        UserBuilder userBuilder;
         if (userFromDb.isPresent()) {
             userBuilder = org.springframework.security.core.userdetails.User
                     .withUsername(email);
