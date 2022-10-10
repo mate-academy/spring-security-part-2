17df140307b784add8250330195bc259e7c74dc1
Oleksii Mospan <alexey.mospan@gmail.com>
2022-10-10 15:51:45 +0300

    Changed parameter in register method to User.

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -5,6 +5,7 @@ import mate.academy.spring.dto.request.UserRequestDto;
 import mate.academy.spring.dto.response.UserResponseDto;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.AuthenticationService;
+import mate.academy.spring.service.mapper.RequestDtoMapper;
 import mate.academy.spring.service.mapper.ResponseDtoMapper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,16 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     private final AuthenticationService authService;
     private final ResponseDtoMapper<UserResponseDto, User> userDtoResponseMapper;
+    private final RequestDtoMapper<UserRequestDto, User> userRequestDtoMapper;
 
     public AuthenticationController(AuthenticationService authService,
-            ResponseDtoMapper<UserResponseDto, User> userDtoResponseMapper) {
+                                    ResponseDtoMapper<UserResponseDto, User> userDtoResponseMapper,
+                                    RequestDtoMapper<UserRequestDto, User> userRequestDtoMapper) {
         this.authService = authService;
         this.userDtoResponseMapper = userDtoResponseMapper;
+        this.userRequestDtoMapper = userRequestDtoMapper;
     }
 
     @PostMapping("/register")
     public UserResponseDto register(@RequestBody @Valid UserRequestDto requestDto) {
-        User user = authService.register(requestDto.getEmail(), requestDto.getPassword());
+        User user = authService.register(userRequestDtoMapper.mapToModel(requestDto));
         return userDtoResponseMapper.mapToDto(user);
     }
 }
