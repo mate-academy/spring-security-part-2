b1e8c23f7b241375e0642ff59d4ec6ce271686df
Oleksii Akishev <akishev.oleksii@gmail.com>
2022-10-10 21:48:46 +0300

    Delete in-memory authentication and add UserDetails

---
+++ src/main/java/mate/academy/spring/service/impl/CustomUserDetailService.java
@@ -0,0 +1,37 @@
+package mate.academy.spring.service.impl;
+
+import mate.academy.spring.model.Role;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+    private UserService userService;
+
+    public CustomUserDetailService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        Optional<User> userOptional = userService.findByEmail(email);
+        UserBuilder builder;
+        if (userOptional.isPresent()) {
+            builder = org.springframework.security.core.userdetails.User.withUsername(email);
+            builder.password(userOptional.get().getPassword());
+            builder.roles(userOptional.get().getRoles()
+                    .stream()
+                    .map(Role::getRoleName)
+                    .toArray(String[]::new));
+            return builder.build();
+        }
+        throw new UsernameNotFoundException("User not found by email: " + email);
+    }
+}
--- src/main/java/mate/academy/spring/service/impl/RoleServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/RoleServiceImpl.java
@@ -3,7 +3,9 @@ package mate.academy.spring.service.impl;
 import mate.academy.spring.dao.RoleDao;
 import mate.academy.spring.model.Role;
 import mate.academy.spring.service.RoleService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RoleServiceImpl implements RoleService {
     private final RoleDao roleDao;
 
