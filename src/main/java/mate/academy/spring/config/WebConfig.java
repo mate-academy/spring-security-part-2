7b952f866352f271c7afa29fe5b903ff49b64a22
Oleksii Mospan <alexey.mospan@gmail.com>
2022-10-11 13:41:29 +0300

    Removed unnecessary implementation of WebMvcConfigurer interface and overrided methods

--- src/main/java/mate/academy/spring/config/WebConfig.java
+++ src/main/java/mate/academy/spring/config/WebConfig.java
@@ -4,27 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "mate.academy.spring.controller")
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/hello").setViewName("hello");
-    }
-
+public class WebConfig {
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver resolver =
