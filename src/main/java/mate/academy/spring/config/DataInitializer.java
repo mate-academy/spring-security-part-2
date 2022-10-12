56a093c217504bc7a0cef02b0bf0ec60c2593475
Oleksii Mospan <alexey.mospan@gmail.com>
2022-10-12 14:11:16 +0300

    Moved RoleName enum inside Role class

--- src/main/java/mate/academy/spring/config/DataInitializer.java
+++ src/main/java/mate/academy/spring/config/DataInitializer.java
@@ -3,7 +3,6 @@ package mate.academy.spring.config;
 import java.util.Set;
 import javax.annotation.PostConstruct;
 import mate.academy.spring.model.Role;
-import mate.academy.spring.model.RoleName;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.AuthenticationService;
 import mate.academy.spring.service.RoleService;
@@ -22,29 +21,29 @@ public class DataInitializer {
     @PostConstruct
     public void inject() {
         Role userRole = new Role();
-        userRole.setName(RoleName.USER);
+        userRole.setName(Role.RoleName.USER);
         roleService.add(userRole);
         Role adminRole = new Role();
-        adminRole.setName(RoleName.ADMIN);
+        adminRole.setName(Role.RoleName.ADMIN);
         roleService.add(adminRole);
 
         User bob = new User();
         bob.setEmail("bob@gmail.com");
         bob.setPassword("12345678");
-        bob.setRoles(Set.of(roleService.getByName(RoleName.USER.name()).get()));
+        bob.setRoles(Set.of(roleService.getByName(Role.RoleName.USER.name()).get()));
         authService.register(bob);
 
         User alice = new User();
         alice.setEmail("alice@gmail.com");
         alice.setPassword("12345678");
-        alice.setRoles(Set.of(roleService.getByName(RoleName.USER.name()).get()));
+        alice.setRoles(Set.of(roleService.getByName(Role.RoleName.USER.name()).get()));
         authService.register(alice);
 
         User admin = new User();
         admin.setEmail("admin@gmail.com");
         admin.setPassword("12345678");
-        admin.setRoles(Set.of(roleService.getByName(RoleName.USER.name()).get(),
-                roleService.getByName(RoleName.ADMIN.name()).get()));
+        admin.setRoles(Set.of(roleService.getByName(Role.RoleName.USER.name()).get(),
+                roleService.getByName(Role.RoleName.ADMIN.name()).get()));
         authService.register(admin);
     }
 }
--- src/main/java/mate/academy/spring/model/Role.java
+++ src/main/java/mate/academy/spring/model/Role.java
@@ -43,4 +43,18 @@ public class Role {
                 + ", name='" + name + '\''
                 + '}';
     }
+
+    public enum RoleName {
+        ADMIN("ADMIN"),
+        USER("USER");
+        private final String name;
+
+        RoleName(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
 }
