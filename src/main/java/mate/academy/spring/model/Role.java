dac719d216b5c74d1533774f193f3b84d438eba5
Oleksandr Semashko <semashko123456789@gmail.com>
2022-10-18 09:32:16 +0300

    added new entity Role

---
+++ src/main/java/mate/academy/spring/model/Role.java
@@ -0,0 +1,33 @@
+package mate.academy.spring.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private RoleName roleName;
+
+    public enum RoleName {
+        ADMIN,
+        USER
+    }
+}
