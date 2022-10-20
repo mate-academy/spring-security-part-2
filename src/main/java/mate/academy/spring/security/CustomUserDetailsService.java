6fcdee07c3cd5e442c57ff05e606606cc19991be
GolDenik <denik.golibroda@gmail.com>
2022-10-20 17:13:53 +0300

    did the task and checked common mistakes, p.s. login doesn't work and I don't know why)

---
+++ src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package mate.academy.spring.security;
+
+import java.util.Optional;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        Optional<User> optionalUser = userService.findByEmail(username);
+        UserBuilder builder;
+        if (optionalUser.isPresent()) {
+            builder = org.springframework.security.core.userdetails.User.withUsername(username);
+            builder.password(optionalUser.get().getPassword());
+            builder.roles(optionalUser.get().getRoles()
+                    .stream()
+                    .map(r -> r.getRoleName().name())
+                    .toArray(String[]::new));
+            return builder.build();
+        }
+        throw new UsernameNotFoundException("User not found.");
+    }
+}
