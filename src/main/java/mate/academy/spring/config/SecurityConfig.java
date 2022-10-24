8dce4a5e7bde7c91493bf0f296a89b96739dd15e
Max Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-10-25 02:49:08 +0300

    Configure access to all endpoints in SecurityConfig

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package mate.academy.spring.config;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,8 +25,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        final String[] adminPaths =
+                {"/cinema-halls", "/movies", "/movie-sessions/**", "/users/by-email"};
+        final String[] adminAndUserPaths =
+                {"/cinema-halls", "/movies", "/movie-sessions/available"};
+        final String[] userPaths = {"/orders/**", "/shopping-carts/**"};
         http
                 .authorizeRequests()
+                .antMatchers("/register").permitAll()
+                .antMatchers(HttpMethod.GET, adminAndUserPaths).hasAnyRole("ADMIN", "USER")
+                .antMatchers(adminPaths).hasRole("ADMIN")
+                .antMatchers(userPaths).hasRole("USER")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
