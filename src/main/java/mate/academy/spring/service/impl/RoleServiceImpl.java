1535b69f7d57667fe608e7bff8f9575d42cbcc96
Anna Popova <anitapopova1996@gmail.com>
2022-10-24 12:54:19 +0300

    Refactored implementation of getByName() method.

--- src/main/java/mate/academy/spring/service/impl/RoleServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/RoleServiceImpl.java
@@ -20,6 +20,9 @@ public class RoleServiceImpl implements RoleService {
 
     @Override
     public Role getByName(String roleName) {
-        return roleDao.getByName(roleName);
+        return roleDao.getByName(roleName).orElseThrow(
+                () -> new RuntimeException("Can't get role: "
+                        + roleName)
+        );
     }
 }
