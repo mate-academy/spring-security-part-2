1416ded65b05819d1315f60d41a9a8d1238631df
LubusUzh <lubus.uzh@gmail.com>
2022-10-25 00:43:16 +0300

    Add: implementation to the Role class, DAO and Service interfaces and classes

--- src/main/java/mate/academy/spring/service/impl/RoleServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/RoleServiceImpl.java
@@ -1,4 +1,22 @@
 package mate.academy.spring.service.impl;
 
-public class RoleServiceImpl {
+import lombok.AllArgsConstructor;
+import mate.academy.spring.dao.RoleDao;
+import mate.academy.spring.model.Role;
+import mate.academy.spring.service.RoleService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RoleServiceImpl implements RoleService {
+    private RoleDao roleDao;
+    @Override
+    public Role add(Role role) {
+        return roleDao.add(role);
+    }
+
+    @Override
+    public Role getByName(String roleName) {
+        return roleDao.getByName(roleName).orElseThrow(() -> new RuntimeException("Can't get role: " + roleName));
+    }
 }
