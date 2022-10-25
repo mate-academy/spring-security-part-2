cd1e7d80a5fbc0215b07c4ebcadd641ffc13b300
Andrii Nesterenko <nesterenkoandrii23@gmail.com>
2022-10-25 09:06:56 +0300

    removed empty line replaced toString on name in loadUserByUsername

--- src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
+++ src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
@@ -20,12 +20,11 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userService.findByEmail(username).orElseThrow(() ->
                 new UsernameNotFoundException("User not found " + username));
-
         UserBuilder builder =
                 org.springframework.security.core.userdetails.User.withUsername(username);
         builder.password(user.getPassword());
         builder.roles(user.getRoles().stream()
-                .map(r -> r.getRoleName().toString()).toArray(String[]::new));
+                .map(r -> r.getRoleName().name()).toArray(String[]::new));
         return builder.build();
     }
 }
