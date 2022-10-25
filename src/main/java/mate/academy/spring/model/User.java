77077eec6e8627ef5c4adf91156eacc2cf6eeff8
Volodymyr Tsupryk <lordvolodua@gmail.com>
2022-10-25 15:11:25 +0300

    fix code stile

--- src/main/java/mate/academy/spring/model/User.java
+++ src/main/java/mate/academy/spring/model/User.java
@@ -1,7 +1,15 @@
 package mate.academy.spring.model;
 
 import java.util.Set;
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "users")
