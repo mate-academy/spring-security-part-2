6c81995a2395ef0d42878140723bf22d44009454
KosatskiyKirill <kirillkosatskiy@gmail.com>
2022-10-25 16:08:52 +0300

    returns previous version.

--- src/main/java/mate/academy/spring/service/mapper/UserMapper.java
+++ src/main/java/mate/academy/spring/service/mapper/UserMapper.java
@@ -1,11 +1,8 @@
 package mate.academy.spring.service.mapper;
 
 import mate.academy.spring.dto.response.UserResponseDto;
-import mate.academy.spring.model.Role;
 import mate.academy.spring.model.User;
 import org.springframework.stereotype.Component;
-
-import java.util.Set;
 
 @Component
 public class UserMapper implements ResponseDtoMapper<UserResponseDto, User> {
