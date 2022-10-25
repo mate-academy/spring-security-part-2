1911f1297254e1fbc77216eceec62636575ff3b8
Vasyl Pankiv <wasilius2@gmail.com>
2022-10-25 19:44:48 +0300

    Add role dao and service layers

---
+++ src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
@@ -0,0 +1,31 @@
+package mate.academy.spring.dao.impl;
+
+import mate.academy.spring.dao.AbstractDao;
+import mate.academy.spring.dao.RoleDao;
+import mate.academy.spring.exception.DataProcessingException;
+import mate.academy.spring.model.Role;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.Optional;
+
+public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
+
+    public RoleDaoImpl(SessionFactory factory) {
+        super(factory, Role.class);
+    }
+
+    @Override
+    public Optional<Role> getByName(String roleName) {
+        try (Session session = factory.openSession()) {
+            Query<Role> roleQuery =
+                    session.createQuery("from Role r where r.name = :roleName", Role.class);
+            Role.RoleName name = Role.RoleName.valueOf(roleName);
+            roleQuery.setParameter("roleName", name);
+            return roleQuery.uniqueResultOptional();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can not find role with name: " + roleName, e);
+        }
+    }
+}
