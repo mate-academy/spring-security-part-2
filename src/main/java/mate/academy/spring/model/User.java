d7bdeb8d5f6425e38dc4c6a362da21085b4605c0
Yevhen Kupriiuk <kupriuk@gmail.com>
2022-10-25 16:17:07 +0300

    to string is deleted

--- src/main/java/mate/academy/spring/model/User.java
+++ src/main/java/mate/academy/spring/model/User.java
@@ -52,12 +52,4 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
-
-    @Override
-    public String toString() {
-        return "User{"
-                + "id=" + id
-                + ", email='" + email
-                + '}';
-    }
 }
