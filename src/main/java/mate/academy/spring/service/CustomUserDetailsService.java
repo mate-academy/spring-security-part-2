27670f280751ebea12f76efe65ee70963ea7b961
LubusUzh <lubus.uzh@gmail.com>
2022-10-25 15:49:12 +0300

    Add: CustomUserDetailsService and implementation

---
+++ src/main/java/mate/academy/spring/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package mate.academy.spring.service;
+
+import java.util.Optional;
+import lombok.AllArgsConstructor;
+import mate.academy.spring.model.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> userOptional = userService.findByEmail(email);
+        UserBuilder userBuilder;
+        if (userOptional.isPresent()) {
+            userBuilder = org.springframework.security.core.userdetails.User.withUsername(email);
+            userBuilder.password(userOptional.get().getPassword());
+            userBuilder.roles(userOptional.get().getRoles()
+                    .stream()
+                    .map(r -> r.getRoleName().name())
+                    .toArray(String[]::new));
+            return userBuilder.build();
+        }
+        throw new UsernameNotFoundException("Can't find user with this email: " + email);
+    }
+}
