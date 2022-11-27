e636169898ce6e5f7de5168cffba6abf9ee5a34b
Oleg Pruh <olegpruh1@gmail.com>
2022-11-27 10:26:11 +0000

    Override deleted and changed role column name

--- src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
@@ -7,35 +7,12 @@ import mate.academy.spring.model.Role;
 import mate.academy.spring.security.RoleName;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
     public RoleDaoImpl(SessionFactory factory) {
         super(factory, Role.class);
-    }
-
-    @Override
-    public Role add(Role role) {
-        Session session = null;
-        Transaction transaction = null;
-        try {
-            session = factory.openSession();
-            transaction = session.beginTransaction();
-            session.save(role);
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            throw new DataProcessingException("Can't add role to db", e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
-        return role;
     }
 
     @Override
--- src/main/java/mate/academy/spring/model/Role.java
+++ src/main/java/mate/academy/spring/model/Role.java
@@ -16,7 +16,7 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Enumerated(EnumType.STRING)
-    @Column(name = "roleName")
+    @Column(name = "role_name")
     private RoleName roleName;
 
     public Role() {
