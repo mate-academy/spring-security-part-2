f74e15cfae11a0f0831a0c6d1ae232c2adb399fb
Vitaliy Ozhelivskiy <ozelivskijvitalij@gmail.com>
2022-12-01 14:19:09 +0200

    refactored

--- src/main/java/mate/academy/spring/service/impl/RoleServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/RoleServiceImpl.java
@@ -1,7 +1,6 @@
 package mate.academy.spring.service.impl;
 
 import mate.academy.spring.dao.RoleDao;
-import mate.academy.spring.exception.DataProcessingException;
 import mate.academy.spring.model.Role;
 import mate.academy.spring.service.RoleService;
 import org.springframework.stereotype.Service;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class RoleServiceImpl implements RoleService {
     private final RoleDao roleDao;
-
     public RoleServiceImpl(RoleDao roleDao) {
         this.roleDao = roleDao;
     }
@@ -21,13 +19,6 @@ public class RoleServiceImpl implements RoleService {
 
     @Override
     public Role getByName(String roleName) {
-        if (!roleName.equals(Role.RoleName.ADMIN.name())
-                && !roleName.equals(Role.RoleName.USER.name())) {
-            throw new DataProcessingException("Role " + roleName
-                    + "didn't find", new RuntimeException());
-        }
-        return roleName.equals(Role.RoleName.USER.name())
-                ? roleDao.getByName(Role.RoleName.USER).get() :
-                roleDao.getByName(Role.RoleName.ADMIN).get();
+        return roleDao.getByName(Role.RoleName.valueOf(roleName)).get();
     }
 }
