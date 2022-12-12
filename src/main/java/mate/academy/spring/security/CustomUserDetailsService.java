d21b8c5db36b5367ecfd388af7e5bc2d9c93f0df
Yura Hryniv <superyura444@gmail.com>
2022-12-12 12:38:16 +0200

    CustomUserDetailsService is implemented

---
+++ src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package mate.academy.spring.security;
+
+import java.util.Optional;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userService.findByEmail(username);
+        if (optionalUser.isPresent()) {
+            org.springframework.security.core.userdetails.User.UserBuilder builder =
+                    org.springframework.security.core.userdetails.User.withUsername(username);
+            builder.password(optionalUser.get().getPassword());
+            builder.roles(optionalUser.get().getRoles()
+                    .stream()
+                    .map(x -> x.getRoleName().name())
+                    .toArray(String[]::new));
+            return builder.build();
+        }
+        throw new UsernameNotFoundException("User not found");
+    }
+}
