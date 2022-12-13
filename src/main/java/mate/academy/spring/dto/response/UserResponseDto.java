9c1a55dfc73850a0b8e84642f17f1b0a25aea64b
Anastasia Vasylyk <anastasiavasylykv@gmail.com>
2022-12-13 13:40:55 +0200

    Spring Security Part 2

--- src/main/java/mate/academy/spring/dto/response/UserResponseDto.java
+++ src/main/java/mate/academy/spring/dto/response/UserResponseDto.java
@@ -1,8 +1,11 @@
 package mate.academy.spring.dto.response;
 
+import java.util.Set;
+
 public class UserResponseDto {
     private Long id;
     private String email;
+    private Set<Long> rolesIds;
 
     public Long getId() {
         return id;
@@ -18,5 +21,13 @@ public class UserResponseDto {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Set<Long> getRolesIds() {
+        return rolesIds;
+    }
+
+    public void setRolesIds(Set<Long> rolesIds) {
+        this.rolesIds = rolesIds;
     }
 }
