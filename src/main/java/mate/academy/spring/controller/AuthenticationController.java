d2d967030356833f9e76562163bb8a42f0302ce9
Maksym Shveda <mshveda28@gmail.com>
2022-12-13 12:24:28 +0200

    Move role initialization from controller to service

--- src/main/java/mate/academy/spring/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/controller/AuthenticationController.java
@@ -1,13 +1,10 @@
 package mate.academy.spring.controller;
 
-import java.util.Set;
 import javax.validation.Valid;
 import mate.academy.spring.dto.request.UserRequestDto;
 import mate.academy.spring.dto.response.UserResponseDto;
-import mate.academy.spring.model.Role;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.AuthenticationService;
-import mate.academy.spring.service.RoleService;
 import mate.academy.spring.service.mapper.ResponseDtoMapper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,25 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticationController {
-    private static final Role.RoleName DEFAULT_ROLE = Role.RoleName.USER;
     private final AuthenticationService authService;
-    private final RoleService roleService;
     private final ResponseDtoMapper<UserResponseDto, User> userDtoResponseMapper;
 
     public AuthenticationController(AuthenticationService authService,
-                                    RoleService roleService,
                                     ResponseDtoMapper<UserResponseDto, User>
                                             userDtoResponseMapper) {
         this.authService = authService;
-        this.roleService = roleService;
         this.userDtoResponseMapper = userDtoResponseMapper;
     }
 
     @PostMapping("/register")
     public UserResponseDto register(@RequestBody @Valid UserRequestDto requestDto) {
         User user = authService.register(requestDto.getEmail(), requestDto.getPassword());
-        Role role = roleService.getByName(DEFAULT_ROLE.name());
-        user.setRoles(Set.of(role));
         return userDtoResponseMapper.mapToDto(user);
     }
 }
