815ad2ec9550bfd930bffa8799522e28c52dc7eb
Artur Markoboh <arthur4655@gmail.com>
2022-12-14 23:15:15 +0200

    Added Role with Dao and Service for it, implemented limited access to urls

---
+++ src/main/java/mate/academy/spring/security/CustomerDetailsService.java
@@ -0,0 +1,32 @@
+package mate.academy.spring.security;
+
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    public CustomerDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userEmail)
+            throws UsernameNotFoundException {
+        User user = userService.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + userEmail
+                        + " not found"));
+        UserBuilder userBuilder =
+                org.springframework.security.core.userdetails.User.withUsername(userEmail);
+        userBuilder.password(user.getPassword());
+        userBuilder.roles(user.getRoles().stream().map(r -> r.getRoleName().name())
+                .toArray(String[]::new));
+        return userBuilder.build();
+    }
+}
