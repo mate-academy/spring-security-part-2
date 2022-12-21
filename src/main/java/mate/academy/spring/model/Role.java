41b309167289a09fa8be0b824e1f26779775c3bb
Veronika Tretiak <nikatretiak9@gmail.com>
2022-12-21 13:35:05 +0100

    task with mistakes

---
+++ src/main/java/mate/academy/spring/model/Role.java
@@ -0,0 +1,58 @@
+package mate.academy.spring.model;
+
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(value = EnumType.STRING)
+    private RoleName role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleName getRole() {
+        return role;
+    }
+
+    public void setRole(RoleName role) {
+        this.role = role;
+    }
+
+    public enum RoleName {
+        USER,
+        ADMIN
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Role roleName = (Role) o;
+        return Objects.equals(id, roleName.id) && role == roleName.role;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, role);
+    }
+}
