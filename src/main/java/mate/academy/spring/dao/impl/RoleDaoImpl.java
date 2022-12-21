adb7bcf447d9ce70190ba0d6e9f319d93e6ba23e
Bohdan Senyk <senyk2001@gmail.com>
2022-12-21 15:58:24 +0200

    Configured DB auth instead of in memory auth Added Role entity, Dao and Service layer for it. Configure role access to specific resources for ADMIN and for USER

---
+++ src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
@@ -0,0 +1,32 @@
+package mate.academy.spring.dao.impl;
+
+import java.util.Optional;
+import mate.academy.spring.dao.AbstractDao;
+import mate.academy.spring.dao.RoleDao;
+import mate.academy.spring.exception.DataProcessingException;
+import mate.academy.spring.model.Role;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
+    public RoleDaoImpl(SessionFactory factory) {
+        super(factory, Role.class);
+    }
+
+    @Override
+    public Optional<Role> getByName(String roleNameString) {
+        try (Session session = factory.openSession()) {
+            Role.RoleName roleName = Role.RoleName.valueOf(roleNameString);
+            Query<Role> findByName = session.createQuery(
+                    "SELECT r FROM Role r "
+                            + "WHERE r.name = :name", Role.class);
+            findByName.setParameter("name", roleName);
+            return findByName.uniqueResultOptional();
+        } catch (Exception e) {
+            throw new DataProcessingException("Role with name " + roleNameString + " not found", e);
+        }
+    }
+}
