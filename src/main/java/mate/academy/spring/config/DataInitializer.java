845c4436843b61ba93d25464aa3bb9d8e3efcdc2
Artem Kurinniy <artem.kyrinniy@gmail.com>
2022-12-22 09:47:03 +0200

    add injection of roles and admin user

--- src/main/java/mate/academy/spring/config/DataInitializer.java
+++ src/main/java/mate/academy/spring/config/DataInitializer.java
@@ -1,5 +1,9 @@
 package mate.academy.spring.config;
 
+import java.util.Set;
+import javax.annotation.PostConstruct;
+import mate.academy.spring.model.Role;
+import mate.academy.spring.model.User;
 import mate.academy.spring.service.RoleService;
 import mate.academy.spring.service.UserService;
 import org.springframework.stereotype.Component;
@@ -15,18 +19,18 @@ public class DataInitializer {
         this.userService = userService;
     }
 
-//    @PostConstruct
-//    public void inject() {
-//        Role adminRole = new Role();
-//        adminRole.setRoleName(Role.RoleName.ADMIN);
-//        roleService.add(adminRole);
-//        Role userRole = new Role();
-//        userRole.setRoleName(Role.RoleName.USER);
-//        roleService.add(userRole);
-//        User user = new User();
-//        user.setEmail("admin@i.ua");
-//        user.setPassword("admin123");
-//        user.setRoles(Set.of(adminRole));
-//        userService.add(user);
-//    }
+    @PostConstruct
+    public void inject() {
+        Role adminRole = new Role();
+        adminRole.setRoleName(Role.RoleName.ADMIN);
+        roleService.add(adminRole);
+        Role userRole = new Role();
+        userRole.setRoleName(Role.RoleName.USER);
+        roleService.add(userRole);
+        User user = new User();
+        user.setEmail("admin@i.ua");
+        user.setPassword("admin123");
+        user.setRoles(Set.of(adminRole));
+        userService.add(user);
+    }
 }
