73b619a5b19ee4c843250f7be37fa2662c5f53c2
Oleg Мarchuk <mar4uk14@gmail.com>
2022-12-22 10:47:49 +0200

    Configure and add model role,Dao and Service layer.

---
+++ src/main/java/mate/academy/spring/model/Role.java
@@ -0,0 +1,42 @@
+package mate.academy.spring.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private Type name;
+
+    public enum Type {
+        ADMIN,USER
+    }
+
+    public Role() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Type getName() {
+        return name;
+    }
+
+    public void setName(Type name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package mate.academy.spring.security;
+
+import static org.springframework.security.core.userdetails.User.withUsername;
+
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private final UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        User user = userService.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + email
+                        + " not found"));
+        org.springframework.security.core.userdetails
+                .User.UserBuilder userBuilder = withUsername(email);
+        userBuilder.password(user.getPassword());
+        userBuilder.roles(user.getRoles().stream().map(r -> r.getName().name())
+                .toArray(String[]::new));
+        return userBuilder.build();
+    }
+}
