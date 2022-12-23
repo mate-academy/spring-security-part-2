11eecb40c30b4f2fed52f3dc2393056fa07647f8
Denys Domashevskyi <denis.domashevskiy28@gmail.com>
2022-12-23 17:00:50 +0200

    Implemented Spring Security

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -1,27 +1,48 @@
 package mate.academy.spring.config;
 
+import static mate.academy.spring.model.Role.RoleName.ADMIN;
+import static mate.academy.spring.model.Role.RoleName.USER;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(UserDetailsService userDetailsService,
+                          PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .passwordEncoder(getEncoder())
-                .withUser("1@123").password(getEncoder().encode("123")).roles("USER");
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/register").permitAll()
+                .antMatchers(HttpMethod.GET,"/cinema-halls").hasAnyRole(ADMIN.name(), USER.name())
+                .antMatchers(HttpMethod.POST,"/cinema-halls").hasRole(ADMIN.name())
+                .antMatchers(HttpMethod.GET,"/movies").hasAnyRole(ADMIN.name(), USER.name())
+                .antMatchers(HttpMethod.POST,"/movies").hasRole(ADMIN.name())
+                .antMatchers("/movie-sessions/**").hasRole(ADMIN.name())
+                .antMatchers(HttpMethod.GET,"/movie-sessions/available").hasRole(USER.name())
+                .antMatchers(HttpMethod.GET,"/orders").hasRole(USER.name())
+                .antMatchers(HttpMethod.POST,"/orders/complete").hasRole(USER.name())
+                .antMatchers("/shopping-carts/movie-sessions").hasRole(USER.name())
+                .antMatchers("/shopping-carts/by-user").hasRole(USER.name())
+                .antMatchers("/users/by-email").hasRole(ADMIN.name())
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -30,10 +51,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .csrf().disable();
-    }
-
-    @Bean
-    public PasswordEncoder getEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
