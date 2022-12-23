aebb50e63be8773ea9cbb59e78dc0119558dfee0
Iryna Chernikova <i.chernikova14@ukr.net>
2022-12-23 18:45:37 +0200

    changed roles() instead of authorities()

--- src/main/java/mate/academy/spring/service/impl/CustomUserDetailsService.java
+++ src/main/java/mate/academy/spring/service/impl/CustomUserDetailsService.java
@@ -25,7 +25,7 @@ public class CustomUserDetailsService implements UserDetailsService {
                         + username));
         UserBuilder builder = withUsername(username);
         builder.password(user.getPassword());
-        builder.authorities(user.getRoles().stream()
+        builder.roles(user.getRoles().stream()
                 .map(r -> r.getRoleName().name())
                 .toArray(String[]::new));
         return builder.build();
