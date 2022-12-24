ef95f933cb95cb976db4b3a0a7112c97583a2827
NSay0 <krvipa@gmail.com>
2022-12-24 14:56:08 +0200

    DB authentication configured. Endpoints access configured. RoleEntity/RoleDao/RoleService implemented.

---
+++ src/main/java/mate/academy/spring/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package mate.academy.spring.service.impl;
+
+import static org.springframework.security.core.userdetails.User.withUsername;
+
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserService userService;
+
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByEmail(username).orElseThrow(
+                () -> new UsernameNotFoundException("Can't find user by username: " + username));
+        return withUsername(username)
+                .password(user.getPassword())
+                .roles(user.getRoles().stream()
+                        .map(r -> r.getRoleName().name())
+                        .toArray(String[]::new))
+                .build();
+    }
+}
