e3213eb262f27e9b67678e689c75a4ac36c4c5cd
AlinaOryshak <alinaoryshak@gmail.com>
2022-12-24 13:04:36 +0200

    Add roles, modify authentication

---
+++ src/main/java/mate/academy/spring/config/DataInitializer.java
@@ -0,0 +1,36 @@
+package mate.academy.spring.config;
+
+import java.util.Set;
+import javax.annotation.PostConstruct;
+import mate.academy.spring.model.Role;
+import mate.academy.spring.model.RoleName;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.RoleService;
+import mate.academy.spring.service.UserService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer {
+    private final UserService userService;
+    private final RoleService roleService;
+
+    public DataInitializer(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @PostConstruct
+    public void inject() {
+        Role adminRole = new Role();
+        adminRole.setRoleName(RoleName.ADMIN);
+        roleService.add(adminRole);
+        Role userRole = new Role();
+        userRole.setRoleName(RoleName.USER);
+        roleService.add(userRole);
+        User user = new User();
+        user.setEmail("admin@i.ua");
+        user.setPassword("admin123");
+        user.setRoles(Set.of(adminRole));
+        userService.add(user);
+    }
+}
