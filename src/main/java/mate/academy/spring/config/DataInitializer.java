31459cdd013ce894ca98c964e4b23749db229c29
Mark <markrybnikov2000@gmail.com>
2022-12-26 12:44:25 +0300

    Created Role model, Role dao and services

---
+++ src/main/java/mate/academy/spring/config/DataInitializer.java
@@ -0,0 +1,32 @@
+package mate.academy.spring.config;
+
+import java.util.Set;
+import javax.annotation.PostConstruct;
+import mate.academy.spring.model.Role;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.RoleService;
+import mate.academy.spring.service.UserService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer {
+    private final UserService userService;
+    private final RoleService roleService;
+
+    public DataInitializer(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @PostConstruct
+    public void inject() {
+        Role adminRole = new Role();
+        adminRole.setName(Role.RoleName.ADMIN);
+        roleService.add(adminRole);
+        User user = new User();
+        user.setEmail("admin8@gmail.com");
+        user.setPassword("admin");
+        user.setRoles(Set.of(adminRole));
+        userService.add(user);
+    }
+}
