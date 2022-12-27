451b050930d0876df0acc39d0e48f97c17442a11
Iryna Chernikova <i.chernikova14@ukr.net>
2022-12-27 11:40:09 +0200

    changed return type to Optional in getByName() in RoleDaoImpl

--- src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
@@ -1,5 +1,6 @@
 package mate.academy.spring.dao.impl;
 
+import java.util.Optional;
 import mate.academy.spring.dao.AbstractDao;
 import mate.academy.spring.dao.RoleDao;
 import mate.academy.spring.exception.DataProcessingException;
@@ -11,18 +12,17 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
-
     public RoleDaoImpl(SessionFactory factory) {
         super(factory, Role.class);
     }
 
     @Override
-    public Role getByName(String roleName) {
+    public Optional<Role> getByName(String roleName) {
         try (Session session = factory.openSession()) {
             Query<Role> getByName = session.createQuery(
                     "FROM Role WHERE roleName = :roleName", Role.class);
             getByName.setParameter("roleName", Role.RoleName.valueOf(roleName));
-            return getByName.uniqueResult();
+            return getByName.uniqueResultOptional();
         } catch (Exception e) {
             throw new DataProcessingException("Role with name " + roleName + " not found", e);
         }
