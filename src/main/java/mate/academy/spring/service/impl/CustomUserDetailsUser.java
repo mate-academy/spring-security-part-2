db0dd4212500d5c073b604ddf7847525850c0bc3
Danyl Kniaziev <kdanil0401@gmail.com>
2022-12-30 11:30:58 +0200

    fixed some style misstakes

--- src/main/java/mate/academy/spring/service/impl/CustomUserDetailsUser.java
+++ src/main/java/mate/academy/spring/service/impl/CustomUserDetailsUser.java
@@ -1,6 +1,7 @@
 package mate.academy.spring.service.impl;
 
 import static org.springframework.security.core.userdetails.User.withUsername;
+
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
 import org.springframework.security.core.userdetails.User.UserBuilder;
