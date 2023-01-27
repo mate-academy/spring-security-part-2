f92c0776624d7342132b4bb8ea3c5a310ae76fda
Kate Chernyavska <ccarts.kpi@gmail.com>
2023-01-27 14:16:05 +0200

    added under field Set<Role> joining columns

--- src/main/java/mate/academy/spring/model/User.java
+++ src/main/java/mate/academy/spring/model/User.java
@@ -1,13 +1,7 @@
 package mate.academy.spring.model;
 
 import java.util.Set;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
@@ -19,6 +13,9 @@ public class User {
     private String email;
     private String password;
     @ManyToMany
+    @JoinTable(name = "user_roles",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roleSet;
 
     public Set<Role> getRoleSet() {
