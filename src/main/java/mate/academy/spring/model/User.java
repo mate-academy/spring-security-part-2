f99e503f4f8091ec6221ca9307672a0fcf99c49a
Kate Chernyavska <ccarts.kpi@gmail.com>
2023-01-27 14:26:25 +0200

    changed importing library instead *

--- src/main/java/mate/academy/spring/model/User.java
+++ src/main/java/mate/academy/spring/model/User.java
@@ -1,7 +1,15 @@
 package mate.academy.spring.model;
 
 import java.util.Set;
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "users")
@@ -14,8 +22,8 @@ public class User {
     private String password;
     @ManyToMany
     @JoinTable(name = "user_roles",
-    joinColumns = @JoinColumn(name = "user_id"),
-    inverseJoinColumns = @JoinColumn(name = "role_id"))
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roleSet;
 
     public Set<Role> getRoleSet() {
