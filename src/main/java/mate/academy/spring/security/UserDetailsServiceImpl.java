e5e8b2d45e6817799bec54408bbe8d44dd969434
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2023-02-06 14:41:20 +0200

    building and refactoring

--- src/main/java/mate/academy/spring/security/UserDetailsServiceImpl.java
+++ src/main/java/mate/academy/spring/security/UserDetailsServiceImpl.java
@@ -9,7 +9,9 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserService userService;
 
