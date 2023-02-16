49edecec941641343a5e7f706bc3aace9c01af91
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-02-16 22:44:20 +0200

    Add Role Getters and Setters

--- src/main/java/mate/academy/spring/model/Role.java
+++ src/main/java/mate/academy/spring/model/Role.java
@@ -15,10 +15,26 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Enumerated(EnumType.STRING)
-    private RoleName roleName;
+    private RoleName name;
 
     public enum RoleName {
         USER,
         ADMIN
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleName getName() {
+        return name;
+    }
+
+    public void setName(RoleName name) {
+        this.name = name;
     }
 }
