65705276f4f0048a4b95112c507cd25b886b3561
Roman Denysiuk <r.v.denysiuk@gmail.com>
2023-02-19 13:56:17 +0200

    changes after review

--- src/main/java/mate/academy/spring/model/Role.java
+++ src/main/java/mate/academy/spring/model/Role.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name = "roles")
@@ -37,4 +38,21 @@ public class Role {
     public void setName(RoleName name) {
         this.name = name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) && name == role.name;
+    }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name);
+        }
 }
