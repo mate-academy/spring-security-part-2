36e63ed003fdcc28a864ac58006bc26fec3842d7
Oleksandr Kostiukevych <kaasnake2@gmail.com>
2023-03-05 16:54:22 +0200

    Spring security2 - 3d commit - bugs fixed

--- src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
@@ -16,11 +16,11 @@ public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
     }
 
     @Override
-    public Role getByName(String roleName) {
+    public Role getByName(Role.RoleName roleName) {
         try (Session session = factory.openSession()) {
             Query<Role> findByEmail = session.createQuery(
                     "FROM Role WHERE name = :name", Role.class);
-            findByEmail.setParameter("name", Role.RoleName.valueOf(roleName));
+            findByEmail.setParameter("name", roleName);
             return findByEmail.uniqueResult();
         } catch (Exception e) {
             throw new DataProcessingException("Role " + roleName + " not found", e);
--- src/main/java/mate/academy/spring/service/mapper/UserMapper.java
+++ src/main/java/mate/academy/spring/service/mapper/UserMapper.java
@@ -1,8 +1,6 @@
 package mate.academy.spring.service.mapper;
 
-import java.util.stream.Collectors;
 import mate.academy.spring.dto.response.UserResponseDto;
-import mate.academy.spring.model.Role;
 import mate.academy.spring.model.User;
 import org.springframework.stereotype.Component;
 
@@ -13,10 +11,6 @@ public class UserMapper implements ResponseDtoMapper<UserResponseDto, User> {
         UserResponseDto responseDto = new UserResponseDto();
         responseDto.setId(user.getId());
         responseDto.setEmail(user.getEmail());
-        responseDto.setRoleIds(user.getRoles()
-                .stream()
-                .map(Role::getId)
-                .collect(Collectors.toSet()));
         return responseDto;
     }
 }
