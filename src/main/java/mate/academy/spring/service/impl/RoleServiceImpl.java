ec7772a7540f24dd529bc4faad4793f424dc56ff
Leonid Topal <leonvil093@gmail.com>
2023-03-06 17:29:13 +0200

    mistakes was fixed

--- src/main/java/mate/academy/spring/service/impl/RoleServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/RoleServiceImpl.java
@@ -19,8 +19,8 @@ public class RoleServiceImpl implements RoleService {
     }
 
     @Override
-    public Role getByName(Role.RoleName roleName) {
-        return roleDao.getByName(roleName).orElseThrow(() ->
+    public Role getByName(String roleName) {
+        return roleDao.getByName(Role.RoleName.valueOf(roleName)).orElseThrow(() ->
                 new RuntimeException("Can't get role by name " + roleName));
     }
 }
