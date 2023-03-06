67fa6b261d3bdf3611485b15e686a11110b77a69
Your NameMykola <mykola.osolinskyi@gmail.com>
2023-03-06 16:07:51 +0100

    add all classes

---
+++ src/main/java/security/CustomUserDetailService.java
@@ -0,0 +1,40 @@
+package security;
+
+import java.util.Optional;
+import mate.academy.spring.model.Role;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+    private final UserService userService;
+
+    public CustomUserDetailService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        Optional<User> userOptional = userService.findByEmail(username);
+        UserBuilder userBuilder;
+        if (userOptional.isEmpty()) {
+            throw new UsernameNotFoundException("Can't found user by email: " + username);
+        }
+        User user = userOptional.get();
+        UserDetails userDetails;
+        org.springframework.security.core.userdetails.User.UserBuilder builder =
+                org.springframework.security.core.userdetails.User.withUsername(username);
+        builder.password(user.getPassword());
+        builder.authorities(user.getRoles()
+                .stream()
+                .map(Role::getRoleName)
+                .toArray(String[]::new));
+        return builder.build();
+    }
+}
