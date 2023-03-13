04521e28d4ba26ea47584f300833f3785754d76d
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-03-13 15:39:50 +0000

    Clean code v 2.0

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package mate.academy.spring.config;
 
 import static mate.academy.spring.model.Role.RoleName.ADMIN;
 import static mate.academy.spring.model.Role.RoleName.USER;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,10 +14,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final UserDetailsService userDetailsService;
-    private final PasswordEncoder passwordEncoder;
     private static final String ADMIN_ROLE_NAME = ADMIN.name();
     private static final String USER_ROLE_NAME = USER.name();
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
 
     public SecurityConfig(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
         this.userDetailsService = userDetailsService;
@@ -33,7 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET,"/register").permitAll()
-                .antMatchers(HttpMethod.GET, "/cinema-halls/**").hasAnyRole(ADMIN_ROLE_NAME,USER_ROLE_NAME)
+                .antMatchers(HttpMethod.GET, "/cinema-halls/**")
+                .hasAnyRole(ADMIN_ROLE_NAME,USER_ROLE_NAME)
                 .antMatchers(HttpMethod.POST, "/cinema-halls/**").hasRole(ADMIN_ROLE_NAME)
                 .antMatchers(HttpMethod.GET, "/movies").hasAnyRole(ADMIN_ROLE_NAME,USER_ROLE_NAME)
                 .antMatchers(HttpMethod.POST, "/movies").hasRole(ADMIN_ROLE_NAME)
@@ -43,8 +45,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.DELETE, "/movie-sessions/**").hasRole(ADMIN_ROLE_NAME)
                 .antMatchers(HttpMethod.GET,"/users/by-email").hasRole(ADMIN_ROLE_NAME)
                 .antMatchers(HttpMethod.GET,"/shopping-carts/**").hasRole(USER_ROLE_NAME)
-                .antMatchers(HttpMethod.GET, "/orders/**").hasAnyRole(ADMIN_ROLE_NAME,USER_ROLE_NAME)
-
+                .antMatchers(HttpMethod.GET, "/orders/**")
+                .hasAnyRole(ADMIN_ROLE_NAME,USER_ROLE_NAME)
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
