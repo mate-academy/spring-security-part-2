31f68ca06f2cd78c1ffef8e9c3a83dbe09096412
Denis Pugovkin <den.pugovkin@gmail.com>
2023-03-22 19:53:33 +0200

    First commit. 1. DB authentication was configured. 2. Role was added in User class. 3. Permissions were added for USER and ADMIN roles.

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -1,39 +1,53 @@
 package mate.academy.spring.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder encoder;
+
+    public SecurityConfig(UserDetailsService userDetailsService,
+                          PasswordEncoder encoder) {
+        this.userDetailsService = userDetailsService;
+        this.encoder = encoder;
+    }
+
     @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .passwordEncoder(getEncoder())
-                .withUser("1@123").password(getEncoder().encode("123")).roles("USER");
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
     }
 
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/register").permitAll()
+                .antMatchers(HttpMethod.POST,"/movies/*").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.POST,"/cinema-halls/*").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.POST,"/movie-sessions/*").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.PUT,"/movie-sessions/*").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.DELETE,"/movie-sessions/*").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.GET,"/users/by-email").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.GET,"/cinema-halls").hasAnyAuthority("ADMIN", "USER")
+                .antMatchers(HttpMethod.GET,"/movies").hasAnyAuthority("ADMIN", "USER")
+                .antMatchers(HttpMethod.GET,"/movie-sessions/available").hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.GET,"/orders").hasAuthority("USER")
+                .antMatchers(HttpMethod.POST,"/orders/*").hasAuthority("USER")
+                .antMatchers(HttpMethod.PUT,"/shopping-carts/movie-sessions").hasAuthority("USER")
+                .antMatchers(HttpMethod.GET,"/shopping-carts/by-user").hasAuthority("USER")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .permitAll()
+                .formLogin().permitAll()
                 .and()
                 .httpBasic()
                 .and()
                 .csrf().disable();
-    }
-
-    @Bean
-    public PasswordEncoder getEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/mate/academy/spring/dao/impl/UserDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/impl/UserDaoImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDaoImpl extends AbstractDao<User> implements UserDao {
+
     public UserDaoImpl(SessionFactory factory) {
         super(factory, User.class);
     }
@@ -20,7 +21,8 @@ public class UserDaoImpl extends AbstractDao<User> implements UserDao {
     public Optional<User> findByEmail(String email) {
         try (Session session = factory.openSession()) {
             Query<User> findByEmail = session.createQuery(
-                    "FROM User WHERE email = :email", User.class);
+                    "FROM User u join fetch u.roles "
+                            + "WHERE u.email = :email", User.class);
             findByEmail.setParameter("email", email);
             return findByEmail.uniqueResultOptional();
         } catch (Exception e) {
