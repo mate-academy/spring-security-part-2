25d0a3679bb7fe277a44125579d7018dd39d1062
Nazarii Kharyshyn <nazariykharyshyn@gmail.com>
2023-03-24 20:41:22 +0200

    spring-security-2 solution -added properties to db.properties and required dependencies to pom.xml file; -created CustomUserDetailsService to configure DB authentication instead of In memory authentication; -created model Role, implemented Dao and Service layers for it; -added field roles for model User and rolesId for UserResponseDto, added required changes in UserMapper; -created DataInitializer to inject roles (USER and ADMIN) and user bob with ADMIN role; -changed AuthenticationServiceImpl in such way that every new registered user has role USER by default; -configured role access to specific resources for ADMIN and for USER

---
+++ src/main/java/mate/academy/spring/service/impl/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package mate.academy.spring.service.impl;
+
+import java.util.Optional;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userService.findByEmail(username);
+        UserBuilder userBuilder;
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            userBuilder = org.springframework.security.core.userdetails.User
+                    .withUsername(user.getEmail());
+            userBuilder.password(user.getPassword());
+            userBuilder.roles(user.getRoles().stream()
+                    .map(role -> role.getRoleName().name())
+                    .toArray(String[]::new));
+            return userBuilder.build();
+        }
+        throw new UsernameNotFoundException("User not found");
+    }
+}
