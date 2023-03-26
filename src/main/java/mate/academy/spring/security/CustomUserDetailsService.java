3de91958d60fc472428dd3d1db33309c9a7fadcb
marjanavv@gmail.com <marjanavv@gmail.com>
2023-03-26 16:49:57 +0200

    Authorisation

---
+++ src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package mate.academy.spring.security;
+
+import java.util.Optional;
+import mate.academy.spring.model.Role;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userOptional = userService.findByEmail(username);
+        if (userOptional.isEmpty()) {
+            throw new UsernameNotFoundException("User is not found !");
+        }
+        User user = userOptional.get();
+        UserBuilder builder =
+                org.springframework.security.core.userdetails.User.withUsername(username);
+        builder.password(user.getPassword());
+        builder.authorities(user.getRoles().stream().map(Role::getRoleName).toArray(String[]::new));
+        return builder.build();
+    }
+}
