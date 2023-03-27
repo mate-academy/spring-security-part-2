4cd752d131f43b6039657b083ce39e797bd4f080
marjanavv@gmail.com <marjanavv@gmail.com>
2023-03-27 15:51:42 +0200

    Equals and hashcode

--- src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
@@ -11,14 +11,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
-
     public RoleDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory, Role.class);
     }
 
     @Override
     public Role getByName(String roleName) {
-
         try (Session session = factory.openSession()) {
             Query<Role> findByRoleName = session.createQuery(
                     "FROM Role WHERE roleName = :roleName", Role.class);
--- src/main/java/mate/academy/spring/model/Role.java
+++ src/main/java/mate/academy/spring/model/Role.java
@@ -1,5 +1,6 @@
 package mate.academy.spring.model;
 
+import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -44,4 +45,20 @@ public class Role {
         this.roleName = roleName;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) && roleName == role.roleName;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, roleName);
+    }
 }
