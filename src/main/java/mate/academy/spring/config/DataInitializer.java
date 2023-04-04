e24e00b217e050d61e3ed4fa516b0465121f829e
vasyl.moshun <vasyl.moshun@gmail.com>
2023-04-04 12:26:05 +0300

    1st

---
+++ src/main/java/mate/academy/spring/config/DataInitializer.java
@@ -0,0 +1,36 @@
+
+package mate.academy.spring.config;
+
+import java.util.Set;
+import javax.annotation.PostConstruct;
+import mate.academy.spring.model.Role;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.RoleService;
+import mate.academy.spring.service.UserService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer {
+    private final RoleService roleService;
+    private final UserService userService;
+
+    public DataInitializer(RoleService roleService, UserService service, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    @PostConstruct
+    public void inject() {
+        Role adminRole = new Role();
+        adminRole.setRoleName(String.valueOf(Role.RoleName.ADMIN));
+        roleService.add(adminRole);
+        Role userRole = new Role();
+        userRole.setRoleName(String.valueOf(Role.RoleName.USER));
+        roleService.add(userRole);
+        User user = new User();
+        user.setEmail("Vasyl@gmail.com");
+        user.setPassword("admin123");
+        user.setRoles(Set.of(adminRole));
+        userService.add(user);
+    }
+}
