2a19d34b6e8c66066872a323b230247ea76b2ad2
“Ymishok321 <kkoiku3@gmail.com>
2023-04-05 17:24:27 +0300

    added roles to project

---
+++ src/main/java/mate/academy/spring/service/impl/CustomUserDetailService.java
@@ -0,0 +1,31 @@
+package mate.academy.spring.service.impl;
+
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+    private UserService userService;
+
+    public CustomUserDetailService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userService.findByEmail(username).orElseThrow(() -> {
+            throw new UsernameNotFoundException("User not found");
+        });
+        org.springframework.security.core.userdetails.User.UserBuilder builder =
+                org.springframework.security.core.userdetails.User.withUsername(username);
+        builder.password(user.getPassword());
+        builder.authorities(user.getRoles().stream()
+                .map(x -> x.getRoleName().name()).toArray(String[]::new));
+        return builder.build();
+    }
+}
