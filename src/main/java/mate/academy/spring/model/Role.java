315fa616f5e81939988991b8aa81fcd47e90cf0b
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-04-23 22:40:52 +0300

    add Role and RoleService

---
+++ src/main/java/mate/academy/spring/model/Role.java
@@ -0,0 +1,47 @@
+package mate.academy.spring.model;
+
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+    @Id
+    Long id;
+    @Enumerated(value = EnumType.STRING)
+    RoleName roleName;
+
+    public Role() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) && roleName == role.roleName;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, roleName);
+    }
+}
