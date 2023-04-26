cb11cbb1869a7499c1a3efe510639b97cb0ae901
codered23 <oleg2003andriy@gmail.com>
2023-04-26 14:30:25 +0300

    changed

--- src/main/java/mate/academy/spring/model/Role.java
+++ src/main/java/mate/academy/spring/model/Role.java
@@ -1,5 +1,6 @@
 package mate.academy.spring.model;
 
+import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -36,5 +37,22 @@ public class Role {
     public enum RoleName {
         USER,
         ADMIN
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Role role = (Role) o;
+        return id.equals(role.id) && roleName == role.roleName;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, roleName);
     }
 }
