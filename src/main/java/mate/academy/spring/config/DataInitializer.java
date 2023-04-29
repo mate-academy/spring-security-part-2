989855ca40900f983c69a663b4239cbdf9527f67
Sergiy Golubchenko <galuba1@gmail.com>
2023-04-29 11:50:58 +0300

    add userRole

--- src/main/java/mate/academy/spring/config/DataInitializer.java
+++ src/main/java/mate/academy/spring/config/DataInitializer.java
@@ -30,7 +30,7 @@ public class DataInitializer {
         User admin = new User();
         admin.setEmail("admin@i.ua");
         admin.setPassword("admin123");
-        admin.setRoles(Set.of(adminRole));
+        admin.setRoles(Set.of(adminRole, userRole));
         userService.add(admin);
 
         User alice = new User();
