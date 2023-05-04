7249a9cc5cffb07422507cea50d8a058be84ab81
Yuriy Turchin <turchin.yuriy1@gmail.com>
2023-05-04 18:34:38 +0300

    implement role identety

---
+++ src/main/java/mate/academy/spring/security/CustomUserDetailService.java
@@ -0,0 +1,36 @@
+package mate.academy.spring.security;
+
+import java.util.Optional;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+    private final UserService userService;
+
+    public CustomUserDetailService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        Optional<User> optionalUser = userService.findByEmail(email);
+        UserBuilder builder;
+        if (optionalUser.isPresent()) {
+            builder = org.springframework.security.core.userdetails.User.withUsername(email);
+            builder.password(optionalUser.get().getPassword());
+            builder.roles(optionalUser.get().getRoles()
+                    .stream()
+                    .map(u -> u.getRole().name())
+                    .toArray(String[]::new));
+            return builder.build();
+        }
+        throw new UsernameNotFoundException("User not found");
+    }
+}
