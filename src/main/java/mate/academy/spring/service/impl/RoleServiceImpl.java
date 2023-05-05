3d5aabd35ced7fba9b06b8cf803a2dfcfd2b4c08
Gleb Iaroshevych <randomeastecho@gmail.com>
2023-05-05 11:55:22 +0300

    Implemented solution for HW.

---
+++ src/main/java/mate/academy/spring/service/impl/RoleServiceImpl.java
@@ -0,0 +1,27 @@
+package mate.academy.spring.service.impl;
+
+import java.util.NoSuchElementException;
+import mate.academy.spring.dao.RoldeDao;
+import mate.academy.spring.model.Role;
+import mate.academy.spring.service.RoleService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+    private final RoldeDao roldeDao;
+
+    public RoleServiceImpl(RoldeDao roldeDao) {
+        this.roldeDao = roldeDao;
+    }
+
+    @Override
+    public Role add(Role role) {
+        return roldeDao.add(role);
+    }
+
+    @Override
+    public Role getByName(Role.RoleName roleName) {
+        return roldeDao.getByName(roleName).orElseThrow(() ->
+                new NoSuchElementException("Can`t get role by role: " + roleName));
+    }
+}
