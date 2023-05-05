4546f3c3f30c6818c3c08c323631bd916e076196
Denys <denchik9026@gmail.com>
2023-05-05 13:24:59 +0300

    configure DB authentication and add role access to resources

---
+++ src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
@@ -0,0 +1,28 @@
+package mate.academy.spring.dao.impl;
+
+import java.util.Optional;
+import mate.academy.spring.dao.AbstractDao;
+import mate.academy.spring.dao.RoleDao;
+import mate.academy.spring.exception.DataProcessingException;
+import mate.academy.spring.model.Role;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
+    public RoleDaoImpl(SessionFactory factory) {
+        super(factory, Role.class);
+    }
+
+    @Override
+    public Optional<Role> getByName(Role.RoleName roleName) {
+        try (Session session = factory.openSession()) {
+            return session.createQuery("from Role r where r.roleName = :roleName", Role.class)
+                    .setParameter("roleName", roleName)
+                    .uniqueResultOptional();
+        } catch (Exception e) {
+            throw new DataProcessingException("No role found by roleName: " + roleName, e);
+        }
+    }
+}
