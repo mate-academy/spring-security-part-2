4831363cab1094ac2a7bc36c37f3d9511f89a0f7
Your Name <you@example.com>
2023-05-05 15:01:58 +0300

    a little fix

--- src/main/java/mate/academy/spring/service/impl/CustomUserDetailsService.java
+++ src/main/java/mate/academy/spring/service/impl/CustomUserDetailsService.java
@@ -2,7 +2,6 @@ package mate.academy.spring.service.impl;
 
 import static org.springframework.security.core.userdetails.User.withUsername;
 
-import mate.academy.spring.model.Role;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
 import org.springframework.security.core.userdetails.User.UserBuilder;
