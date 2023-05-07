cf8c31ff8c3e0e8a39cad8cb316d1db29aab24b8
Mykyta Nikonets <nikita.nikonets@gmail.com>
2023-05-08 02:11:34 +0300

    implemented authorization by roles

--- src/main/java/mate/academy/spring/service/mapper/UserMapper.java
+++ src/main/java/mate/academy/spring/service/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package mate.academy.spring.service.mapper;
 
+import java.util.stream.Collectors;
 import mate.academy.spring.dto.response.UserResponseDto;
 import mate.academy.spring.model.User;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,9 @@ public class UserMapper implements ResponseDtoMapper<UserResponseDto, User> {
         UserResponseDto responseDto = new UserResponseDto();
         responseDto.setId(user.getId());
         responseDto.setEmail(user.getEmail());
+        responseDto.setRoles(user.getRoles().stream()
+                .map(r -> r.getRoleName().name())
+                .collect(Collectors.toList()));
         return responseDto;
     }
 }
