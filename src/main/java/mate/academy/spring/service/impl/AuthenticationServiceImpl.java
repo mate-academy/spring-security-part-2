0b6ec042900bdaa526e3a3d1230cb15b9d393bde
Oleksandr Krasnov <redman2775@gmail.com>
2023-05-08 05:48:18 +0300

    1. DB auth - configured; 2. Role - added; 3. Access to endpoints - configured; 4. Maven checkstyle - BUILD SUCCESS;

--- src/main/java/mate/academy/spring/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/AuthenticationServiceImpl.java
@@ -1,20 +1,26 @@
 package mate.academy.spring.service.impl;
 
+import java.util.Set;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.AuthenticationService;
+import mate.academy.spring.service.RoleService;
 import mate.academy.spring.service.ShoppingCartService;
 import mate.academy.spring.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final String DEFAULT_ROLE = "USER";
     private final UserService userService;
     private final ShoppingCartService shoppingCartService;
+    private final RoleService roleService;
 
     public AuthenticationServiceImpl(UserService userService,
-                                     ShoppingCartService shoppingCartService) {
+                                     ShoppingCartService shoppingCartService,
+                                     RoleService roleService) {
         this.userService = userService;
         this.shoppingCartService = shoppingCartService;
+        this.roleService = roleService;
     }
 
     @Override
@@ -22,6 +28,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         User user = new User();
         user.setEmail(email);
         user.setPassword(password);
+        user.setRoles(Set.of(roleService.getByName(DEFAULT_ROLE)));
         userService.add(user);
         shoppingCartService.registerNewShoppingCart(user);
         return user;
