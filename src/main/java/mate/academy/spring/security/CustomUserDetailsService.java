628df44e4f68c9ab003794038110d76ac5d3c3da
Ditkovskyi Pasha <pashkadit@gmail.com>
2023-05-10 01:41:12 +0300

    Imports checkstyle fix

--- src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
+++ src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
@@ -3,11 +3,11 @@ package mate.academy.spring.security;
 import java.util.Optional;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
