6de28427c2ea6f9e916b49d77338d746eb48b878
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2023-05-18 01:43:54 +0300

    Dao and service for Role were implemented, UserDao were fixed

--- src/main/java/mate/academy/spring/config/DataInitializer.java
+++ src/main/java/mate/academy/spring/config/DataInitializer.java
@@ -1,16 +1,14 @@
 package mate.academy.spring.config;
 
-import mate.academy.spring.model.Role;
-import mate.academy.spring.model.User;
-import mate.academy.spring.service.AuthenticationService;
-import mate.academy.spring.service.RoleService;
-import mate.academy.spring.service.UserService;
 import java.util.Set;
 import javax.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import mate.academy.spring.model.Role;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.RoleService;
+import mate.academy.spring.service.UserService;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataInitializer {
     private UserService userService;
     private RoleService roleService;
@@ -28,10 +26,15 @@ public class DataInitializer {
         Role userRole = new Role();
         userRole.setRoleName(Role.RoleName.USER);
         roleService.add(userRole);
-        User user = new User();
-        user.setEmail("admin@i.ua");
-        user.setPassword("admin123");
-        user.setRoles(Set.of(adminRole));
-        userService.add(user);
+        User adminUser = new User();
+        adminUser.setEmail("admin@i.ua");
+        adminUser.setPassword("admin123");
+        adminUser.setRoles(Set.of(adminRole));
+        userService.add(adminUser);
+        User usualUser = new User();
+        usualUser.setEmail("usualUser@i.ua");
+        usualUser.setPassword("1234test");
+        usualUser.setRoles(Set.of(userRole));
+        userService.add(usualUser);
     }
 }
--- src/main/java/mate/academy/spring/service/impl/RoleServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/RoleServiceImpl.java
@@ -20,7 +20,7 @@ public class RoleServiceImpl implements RoleService {
 
     @Override
     public Role getByName(String roleName) {
-        return roleDao.getByName(roleName)
+        return roleDao.getByName(Role.RoleName.valueOf(roleName))
                 .orElseThrow(() -> new RuntimeException("Can't get role by roleName " + roleName));
     }
 }
