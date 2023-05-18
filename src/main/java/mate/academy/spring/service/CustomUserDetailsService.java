fb1b0029d92292707891af49976dc5c8f4b5a267
Mariana Polishchuk <tonksmaryan@gmail.com>
2023-05-18 13:40:01 +0300

    [HW-SPRING-SECURITY-2] configure DB authentication

---
+++ src/main/java/mate/academy/spring/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package mate.academy.spring.service;
+
+import static org.springframework.security.core.userdetails.User.UserBuilder;
+import static org.springframework.security.core.userdetails.User.withUsername;
+
+import mate.academy.spring.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userService.findByEmail(username).orElseThrow(()
+                -> new UsernameNotFoundException("Can't find user by username " + username));
+        UserBuilder userBuilder = withUsername(username);
+        userBuilder.password(user.getPassword());
+        userBuilder.authorities(user.getRoles()
+                .stream()
+                .map(r -> r.getRoleName().name())
+                .toArray(String[]::new));
+        return userBuilder.build();
+    }
+}
