99e3057be7a2a576b2346f0902aa30bac23db726
Victoria Kharlanchuk <kaplabetty@gmail.com>
2023-06-07 17:54:53 +0200

    checkstyle

--- src/main/java/mate/academy/spring/service/impl/CustomUserDetailsService.java
+++ src/main/java/mate/academy/spring/service/impl/CustomUserDetailsService.java
@@ -1,7 +1,6 @@
 package mate.academy.spring.service.impl;
 
 import java.util.Optional;
-import mate.academy.spring.model.Role;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
