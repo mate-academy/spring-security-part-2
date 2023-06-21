2fde53ae90c82e956bf0b670e1f3554a11fb2912
Andrii Shevchuk <zovten156@gmail.com>
2023-06-21 15:28:55 +0300

    Configured DB

---
+++ src/main/java/mate/academy/spring/config/DataInitializer.java
@@ -0,0 +1,33 @@
+package mate.academy.spring.config;
+
+import java.util.Set;
+import javax.annotation.PostConstruct;
+import mate.academy.spring.model.Role;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.RoleService;
+import mate.academy.spring.service.UserService;
+
+public class DataInitializer {
+    private UserService userService;
+    private RoleService roleService;
+
+    public DataInitializer(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @PostConstruct
+    public void inject() {
+        Role adminRole = new Role();
+        adminRole.setRoleName(Role.RoleName.ADMIN);
+        roleService.add(adminRole);
+        Role userRole = new Role();
+        userRole.setRoleName(Role.RoleName.USER);
+        roleService.add(userRole);
+        User admin = new User();
+        admin.setEmail("admin@gmaiL.com");
+        admin.setPassword("password");
+        admin.setRoles(Set.of(adminRole));
+        userService.add(admin);
+    }
+}
---
+++ src/main/java/mate/academy/spring/model/Role.java
@@ -0,0 +1,63 @@
+package mate.academy.spring.model;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+    @Id
+    private Long id;
+    @Column(name = "role_name")
+    @Enumerated(value = EnumType.STRING)
+    private RoleName roleName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
+
+    public enum RoleName {
+        ADMIN,
+        USER
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Role role = (Role) o;
+        return roleName == role.roleName;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleName);
+    }
+
+    @Override
+    public String toString() {
+        return "Role{"
+                + "id=" + id
+                + ", roleName=" + roleName
+                + '}';
+    }
+}
