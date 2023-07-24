86fe7732e1f373b9bc0c283d5d507b70439fb758
viprko <pr.and.vi@gmail.com>
2023-07-24 19:46:21 +0300

    Configured DB authentication, added Role model

---
+++ src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
@@ -0,0 +1,29 @@
+package mate.academy.spring.dao.impl;
+
+import static mate.academy.spring.model.Role.RoleName;
+
+import java.util.Optional;
+import mate.academy.spring.dao.AbstractDao;
+import mate.academy.spring.dao.RoleDao;
+import mate.academy.spring.exception.DataProcessingException;
+import mate.academy.spring.model.Role;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
+    public RoleDaoImpl(SessionFactory factory) {
+        super(factory, Role.class);
+    }
+
+    @Override
+    public Optional<Role> getByName(RoleName roleName) {
+        try (Session session = factory.openSession()) {
+            return session.createQuery("from Role where roleName = :roleName", Role.class)
+                    .setParameter("roleName", roleName).uniqueResultOptional();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get role by name: " + roleName, e);
+        }
+    }
+}
