90648868a76e84cc7025c0850f01054822ebb681
Gamemod13 <igorfetov1997@gmail.com>
2023-07-26 13:52:13 +0300

    Create first version of implementation

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -1,27 +1,57 @@
 package mate.academy.spring.config;
 
+import mate.academy.spring.model.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .passwordEncoder(getEncoder())
-                .withUser("1@123").password(getEncoder().encode("123")).roles("USER");
+    private static final String ROLE_USER = Role.RoleName.USER.name();
+    private static final String ROLE_ADMIN = Role.RoleName.ADMIN.name();
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(UserDetailsService userDetailsService,
+                          PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    protected void configure(HttpSecurity http) throws Exception {
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/register").permitAll()
+                .antMatchers(
+                        HttpMethod.GET, "/cinema-halls", "/movies", "/movie-sessions/available"
+                )
+                .hasAnyRole(ROLE_USER, ROLE_ADMIN)
+                .antMatchers(HttpMethod.GET, "/users/by-email")
+                .hasRole(ROLE_ADMIN)
+                .antMatchers(HttpMethod.POST, "/cinema-halls", "/movies", "/movie-sessions")
+                .hasRole(ROLE_ADMIN)
+                .antMatchers(HttpMethod.PUT, "/movie-sessions/*")
+                .hasRole(ROLE_ADMIN)
+                .antMatchers(HttpMethod.DELETE, "/movie-sessions/*")
+                .hasRole(ROLE_ADMIN)
+                .antMatchers(HttpMethod.GET, "/orders", "/shopping-carts/by-user")
+                .hasRole(ROLE_USER)
+                .antMatchers(HttpMethod.POST, "/orders/complete")
+                .hasRole(ROLE_USER)
+                .antMatchers(HttpMethod.PUT, "/shopping-carts/movie-sessions")
+                .hasRole(ROLE_USER)
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
---
+++ src/main/java/mate/academy/spring/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,36 @@
+package mate.academy.spring.service.impl;
+
+import static org.springframework.security.core.userdetails.User.builder;
+
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserService userService;
+
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user =
+                userService.findByEmail(username).orElseThrow(
+                        () -> new UsernameNotFoundException("Can't find user with email: "
+                                + username));
+        String[] roles = user.getRoles().stream()
+                .map(r -> r.getRoleName().name())
+                .toArray(String[]::new);
+        return builder()
+                .username(username)
+                .password(user.getPassword())
+                .roles(roles)
+                .build();
+    }
+}
