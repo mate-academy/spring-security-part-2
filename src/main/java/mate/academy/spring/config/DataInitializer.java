b1397ef38faa1165d940c27fac332bf29e8b997d
Andrii <subashandrii@gmail.com>
2023-08-09 23:41:10 +0300

    hw / spring-security-part-2 / fix

--- src/main/java/mate/academy/spring/config/DataInitializer.java
+++ src/main/java/mate/academy/spring/config/DataInitializer.java
@@ -1,21 +1,19 @@
 package mate.academy.spring.config;
 
-import java.util.Set;
 import javax.annotation.PostConstruct;
 import mate.academy.spring.model.Role;
-import mate.academy.spring.model.User;
+import mate.academy.spring.service.AuthenticationService;
 import mate.academy.spring.service.RoleService;
-import mate.academy.spring.service.UserService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataInitializer {
-    private final UserService userService;
     private final RoleService roleService;
+    private final AuthenticationService authenticationService;
 
-    public DataInitializer(UserService userService, RoleService roleService) {
-        this.userService = userService;
+    public DataInitializer(RoleService roleService, AuthenticationService authenticationService) {
         this.roleService = roleService;
+        this.authenticationService = authenticationService;
     }
 
     @PostConstruct
@@ -26,10 +24,6 @@ public class DataInitializer {
         Role userRole = new Role();
         userRole.setRoleName(Role.RoleName.USER);
         roleService.add(userRole);
-        User user = new User();
-        user.setEmail("admin@i.ua");
-        user.setPassword("admin123");
-        user.setRoles(Set.of(adminRole));
-        userService.add(user);
+        authenticationService.register("admin@i.ua", "admin123");
     }
 }
