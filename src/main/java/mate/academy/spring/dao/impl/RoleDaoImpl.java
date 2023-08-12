59e26e6c93e47e9ea278b80a9dd5c38ed0822b71
Vlad Polikarpov <vladpolikarpovvp@gmail.com>
2023-08-12 21:15:44 +0300

    added proper solution for this task

---
+++ src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
@@ -0,0 +1,31 @@
+package mate.academy.spring.dao.impl;
+
+import java.util.Optional;
+import mate.academy.spring.dao.AbstractDao;
+import mate.academy.spring.dao.RoleDao;
+import mate.academy.spring.exception.DataProcessingException;
+import mate.academy.spring.model.Role;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
+
+    @Autowired
+    protected RoleDaoImpl(SessionFactory sessionFactory) {
+        super(sessionFactory, Role.class);
+    }
+
+    @Override
+    public Optional<Role> getByName(String roleName) {
+        try (Session session = factory.openSession()) {
+            return session.createQuery("select r from Role r "
+                            + "where r.roleName = :roleName", Role.class)
+                    .setParameter("roleName", Role.RoleName.valueOf(roleName))
+                    .uniqueResultOptional();
+        } catch (Exception e) {
+            throw new DataProcessingException("Couldn't get role by role name: "
+                    + roleName, e);
+        }
+    }
+}
