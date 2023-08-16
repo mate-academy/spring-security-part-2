fa3957baa1bcd5f318d403176a6885c00aa22131
Serhii Dudko <nechupara@gmail.com>
2023-08-16 23:32:07 +0300

    implemented first version of spring-security-2

---
+++ src/main/java/mate/academy/spring/dao/impl/RoleDaoImpl.java
@@ -0,0 +1,30 @@
+package mate.academy.spring.dao.impl;
+
+import java.util.Optional;
+import mate.academy.spring.dao.AbstractDao;
+import mate.academy.spring.dao.RoleDao;
+import mate.academy.spring.exception.DataProcessingException;
+import mate.academy.spring.model.Role;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
+    public RoleDaoImpl(SessionFactory sessionFactory) {
+        super(sessionFactory, Role.class);
+    }
+
+    @Override
+    public Optional<Role> getByRoleName(Role.RoleName roleName) {
+        try (Session session = factory.openSession()) {
+            Query<Role> findByRole = session.createQuery(
+                    "FROM Role WHERE roleName = :roleName", Role.class);
+            findByRole.setParameter("roleName", roleName);
+            return findByRole.uniqueResultOptional();
+        } catch (Exception e) {
+            throw new DataProcessingException("Role with role name: " + roleName + " not found", e);
+        }
+    }
+}
