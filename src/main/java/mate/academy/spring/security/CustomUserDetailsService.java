17740b87ad50f5e9609f004c481ab25751682a58
Serhii Dudko <nechupara@gmail.com>
2023-08-17 00:17:55 +0300

    added equals and hashCode in Role, orginized import in UsrDtlsService

--- src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
+++ src/main/java/mate/academy/spring/security/CustomUserDetailsService.java
@@ -1,8 +1,11 @@
 package mate.academy.spring.security;
 
+import static org.springframework.security.core.userdetails.User.withUsername;
+
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -21,8 +24,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userService.findByEmail(username).orElseThrow(() ->
                 new UsernameNotFoundException("User with username: " + username + " not found."));
-        org.springframework.security.core.userdetails.User.UserBuilder builder =
-                org.springframework.security.core.userdetails.User.withUsername(username);
+        UserBuilder builder = withUsername(username);
         builder.password(user.getPassword());
         builder.roles(user.getRoles().stream()
                 .map(r -> r.getRoleName().name())
