6cf9bd124a872ee6cc83c39f28a608aa0c5b5add
Artem Kostenko <a_kostenco@yahoo.com>
2023-08-19 19:54:33 +0300

    fix after code review

--- src/main/java/mate/academy/spring/dao/impl/UserDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/impl/UserDaoImpl.java
@@ -33,7 +33,7 @@ public class UserDaoImpl extends AbstractDao<User> implements UserDao {
         try (Session session = factory.openSession()) {
             Query<User> getUser = session.createQuery(
                     "FROM User u "
-                            + "INNER JOIN FETCH u.roles"
+                            + "INNER JOIN FETCH u.roles "
                             + "WHERE u.id = :id", User.class);
             getUser.setParameter("id", id);
             return getUser.uniqueResultOptional();
